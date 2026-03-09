3fce11923766676d33bec30e76c4eb3b34c90874
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-09 18:13:12 +0200

    Tasks 3 and 4 uploaded.

---
+++ BasicSyntax-MoreEx/03.GamingStore/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _03.GamingStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> games = new()
+            {
+                { "OutFall 4", 39.99 },
+                { "CS: OG", 15.99 },
+                { "Zplinter Zell", 19.99 },
+                { "Honored 2", 59.99 },
+                { "RoverWatch", 29.99 },
+                { "RoverWatch Origins Edition", 39.99 }
+            };
+
+            double budget = double.Parse(Console.ReadLine());
+            double currentBalance = budget;
+
+            while (true)
+            {
+                string game = Console.ReadLine();
+
+                if (game == "Game Time")
+                {
+                    double totalSpent = budget - currentBalance;
+                    Console.WriteLine($"Total spent: ${totalSpent:F2}. Remaining: ${currentBalance:F2}");
+                    break;
+                }
+
+                if (games.ContainsKey(game) == false)
+                {
+                    Console.WriteLine("Not Found");
+                    continue;
+                }
+
+                double currentPrice = games[game];
+
+                if (currentPrice > currentBalance)
+                {
+                    Console.WriteLine("Too Expensive");
+                    continue;
+                }
+                else
+                {
+                    currentBalance -= currentPrice;
+                    Console.WriteLine($"Bought {game}");
+                }
+
+                if (currentBalance == 0)
+                {
+                    Console.WriteLine("Out of money!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ BasicSyntax-MoreEx/04.ReverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            string outputText = string.Empty;
+
+            for (int i = inputText.Length - 1; i >= 0; i--)
+            {
+                outputText += inputText[i];
+            }
+
+            Console.WriteLine(outputText);
+        }
+    }
+}
