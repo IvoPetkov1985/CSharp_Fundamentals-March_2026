29fcaf9aa863d42de663d72c937dfbf0a8bf0d5d
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-09 18:11:04 +0200

    Tasks 1 and 2 uploaded.

---
+++ BasicSyntax-MoreEx/01.SortNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _01.SortNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int[] ints = { firstNumber, secondNumber, thirdNumber };
+            Console.WriteLine(string.Join(Environment.NewLine, ints.OrderByDescending(x => x)));
+        }
+    }
+}
---
+++ BasicSyntax-MoreEx/02.EnglishNameOfTheLastDigit/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _02.EnglishNameOfTheLastDigit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpelling(inputNumber));
+        }
+
+        static string GetSpelling(int number)
+        {
+            int lastDigit = number % 10;
+
+            switch (lastDigit)
+            {
+                case 1: return "one";
+                case 2: return "two";
+                case 3: return "three";
+                case 4: return "four";
+                case 5: return "five";
+                case 6: return "six";
+                case 7: return "seven";
+                case 8: return "eight";
+                case 9: return "nine";
+                case 0: return "zero";
+                default: return string.Empty;
+            }
+        }
+    }
+}
