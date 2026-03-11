b99435e130883a11c210d3c3f0bedfd891351ab4
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-11 12:22:29 +0200

    Tasks 1 and 2 uploaded.

---
+++ DataTypes-Lab/01.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01.ConvertMetersToKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int distanceInMeters = int.Parse(Console.ReadLine());
+
+            float distanceInKm = distanceInMeters / 1000f;
+            Console.WriteLine($"{distanceInKm:F2}");
+        }
+    }
+}
---
+++ DataTypes-Lab/02.PoundsToDollars/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.PoundsToDollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+
+            decimal poundRate = 1.31m;
+            decimal sumInDollars = pounds * poundRate;
+
+            Console.WriteLine($"{sumInDollars:F3}");
+        }
+    }
+}
