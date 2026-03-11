6e4ce83ef616d21e8895dab0b6b36814e81d1833
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-11 12:24:27 +0200

    Tasks 5 and 6 uploaded.

---
+++ DataTypes-Lab/05.SpecialNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05.SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (int number = 1; number <= inputNumber; number++)
+            {
+                int numberCopy = number;
+                int sumDigits = 0;
+
+                while (numberCopy > 0)
+                {
+                    int digit = numberCopy % 10;
+                    sumDigits += digit;
+                    numberCopy /= 10;
+                }
+
+                if (sumDigits == 5 || sumDigits == 7 || sumDigits == 11)
+                {
+                    Console.WriteLine($"{number} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} -> False");
+                }
+            }
+        }
+    }
+}
