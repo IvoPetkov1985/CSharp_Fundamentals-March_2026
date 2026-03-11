0def1eb02d8cd8c119051a4e6a1dbd4d5231368a
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-11 12:23:33 +0200

    Tasks 3 and 4 uploaded.

---
+++ DataTypes-Lab/03.ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.ExactSumOfRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            decimal sum = 0m;
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                decimal inputNumber = decimal.Parse(Console.ReadLine());
+                sum += inputNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
