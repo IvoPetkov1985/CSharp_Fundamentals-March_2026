6a0be68d980fd96d2735ae8fffc181cdf9e1d401
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-12 10:42:55 +0200

    Tasks 11 and 12 uploaded.
    .sln file added.

---
+++ DataTypes-Lab/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _12.RefactorSpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int upperLimit = int.Parse(Console.ReadLine());
+
+            for (int number = 1; number <= upperLimit; number++)
+            {
+                int sumDigits = 0;
+                int copy = number;
+
+                while (copy > 0)
+                {
+                    sumDigits += copy % 10;
+                    copy /= 10;
+                }
+
+                bool isSpecialNum = sumDigits == 5 || sumDigits == 7 || sumDigits == 11;
+                Console.WriteLine("{0} -> {1}", number, isSpecialNum);
+            }
+
+        }
+    }
+}
