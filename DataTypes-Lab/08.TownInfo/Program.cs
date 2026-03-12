f03b862efa228003b0a51b1e9b71d725fbdb1b85
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-12 10:41:20 +0200

    Tasks 7 and 8 uploaded.

---
+++ DataTypes-Lab/08.TownInfo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _08.TownInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string townName = Console.ReadLine();
+            uint population = uint.Parse(Console.ReadLine());
+            ushort areaInSqKm = ushort.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {townName} has population of {population} and area {areaInSqKm} square km.");
+        }
+    }
+}
