e9689777e3d0f594e0d94aea56014be74d2aa828
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-13 11:46:33 +0200

    Tasks 5 and 6 uploaded.

---
+++ DataTypes-Exercise/05.PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _05.PrintPartOfASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+
+            for (char c = (char)startNumber; c < (char)endNumber; c++)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine((char)endNumber);
+        }
+    }
+}
---
+++ DataTypes-Exercise/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _06.TriplesOfLatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            int startLetterNum = 97;
+
+            for (int a1 = startLetterNum; a1 < startLetterNum + inputNumber; a1++)
+            {
+                for (int a2 = startLetterNum; a2 < startLetterNum + inputNumber; a2++)
+                {
+                    for (int a3 = startLetterNum; a3 < startLetterNum + inputNumber; a3++)
+                    {
+                        Console.WriteLine($"{(char)a1}{(char)a2}{(char)a3}");
+                    }
+                }
+            }
+        }
+    }
+}
