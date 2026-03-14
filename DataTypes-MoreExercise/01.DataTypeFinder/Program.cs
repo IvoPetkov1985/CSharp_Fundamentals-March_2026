3accdaf96d24208b259b41800a2ee4869aefe1ff
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-14 15:49:12 +0200

    Tasks 1 and 2 uploaded.
    A very fancy solution of task 2.

---
+++ DataTypes-MoreExercise/01.DataTypeFinder/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _01.DataTypeFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string inputLine = Console.ReadLine();
+
+                if (inputLine == "END")
+                {
+                    break;
+                }
+
+                bool isInteger = int.TryParse(inputLine, out int value);
+                bool isDouble = double.TryParse(inputLine, out double db);
+                bool isChar = char.TryParse(inputLine, out char c);
+                bool isBool = bool.TryParse(inputLine, out bool b);
+
+                string outputType = string.Empty;
+
+                if (isInteger == true)
+                {
+                    outputType = "integer";
+                }
+                else if (isDouble == true)
+                {
+                    outputType = "floating point";
+                }
+                else if (isChar == true)
+                {
+                    outputType = "character";
+                }
+                else if (isBool == true)
+                {
+                    outputType = "boolean";
+                }
+                else
+                {
+                    outputType = "string";
+                }
+
+                Console.WriteLine($"{inputLine} is {outputType} type");
+            }
+        }
+    }
+}
---
+++ DataTypes-MoreExercise/02.FromLeftToTheRight/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+
+namespace _02.FromLeftToTheRight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int line = 0; line < numberOfLines; line++)
+            {
+                string input = Console.ReadLine();
+
+                BigInteger[] numbers = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(BigInteger.Parse)
+                    .ToArray();
+
+                BigInteger leftNumber = numbers.First();
+                BigInteger rightNumber = numbers.Last();
+                BigInteger maxNumber = BigInteger.Max(leftNumber, rightNumber);
+
+                string numAsString = BigInteger.Abs(maxNumber).ToString();
+                int sumDigits = 0;
+
+                for (int i = 0; i < numAsString.Length; i++)
+                {
+                    int digit = int.Parse(numAsString[i].ToString());
+                    sumDigits += digit;
+                }
+
+                Console.WriteLine(sumDigits);
+            }
+        }
+    }
+}
