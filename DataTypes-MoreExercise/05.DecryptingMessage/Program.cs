fd19f5232c0a452efcba1d74063c65b1ccc9118b
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-14 15:50:42 +0200

    Tasks 5 and 6 uploaded.

---
+++ DataTypes-MoreExercise/05.DecryptingMessage/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.DecryptingMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int numberOfLines = int.Parse(Console.ReadLine());
+            string message = string.Empty;
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                letter += (char)key;
+                message += letter;
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ DataTypes-MoreExercise/06.BalancedBrackets/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _06.BalancedBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            string open = string.Empty;
+            int openCounter = 0;
+            int closeCounter = 0;
+            bool isValid = true;
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "(" && open != "(")
+                {
+                    open = "(";
+                    openCounter++;
+                }
+                else if (line == "(" && open == "(")
+                {
+                    isValid = false;
+                    break;
+                }
+                else if (line == ")" && open == "(")
+                {
+                    open = string.Empty;
+                    closeCounter++;
+                }
+                else if (line == ")" && open != "(")
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid && openCounter == closeCounter)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
