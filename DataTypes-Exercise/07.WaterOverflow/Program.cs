9f24a72446f8f7d5f7a2e8298da458c16b4681c5
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-15 11:33:28 +0200

    Tasks 7 and 8 uploaded.

---
+++ DataTypes-Exercise/07.WaterOverflow/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte linesCount = byte.Parse(Console.ReadLine());
+            ushort water = 0;
+            ushort capacity = 255;
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                ushort quantity = ushort.Parse(Console.ReadLine());
+
+                if (quantity > (capacity - water))
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    water += quantity;
+                }
+            }
+
+            Console.WriteLine(water);
+        }
+    }
+}
---
+++ DataTypes-Exercise/08.BeerKegs/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _08.BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte kegsCount = byte.Parse(Console.ReadLine());
+
+            string biggestKegModel = string.Empty;
+            double biggestKegVolume = 0;
+
+            for (int i = 0; i < kegsCount; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double currentKegVolume = Math.PI * Math.Pow(radius, 2) * height;
+
+                if (currentKegVolume > biggestKegVolume)
+                {
+                    biggestKegModel = model;
+                    biggestKegVolume = currentKegVolume;
+                }
+            }
+
+            Console.WriteLine(biggestKegModel);
+        }
+    }
+}
