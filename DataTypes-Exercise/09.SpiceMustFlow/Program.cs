3258b6d75a1e38a08a1c8e65d82c9c982a0a2a43
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-15 11:34:01 +0200

    Tasks 9 and 10 uploaded.

---
+++ DataTypes-Exercise/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _09.SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint dailyYield = uint.Parse(Console.ReadLine());
+
+            uint days = 0;
+            uint totalYield = 0;
+
+            while (dailyYield >= 100)
+            {
+                totalYield += dailyYield;
+                totalYield -= 26;
+                days++;
+                dailyYield -= 10;
+            }
+
+            if (totalYield >= 26)
+            {
+                totalYield -= 26;
+            }
+            else
+            {
+                totalYield = 0;
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(totalYield);
+        }
+    }
+}
---
+++ DataTypes-Exercise/10.Pokemon/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _10.Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint pokePower = uint.Parse(Console.ReadLine());
+            uint distance = uint.Parse(Console.ReadLine());
+            uint exhaustionFactor = uint.Parse(Console.ReadLine());
+
+            double halfPower = pokePower * 0.5d;
+            uint counter = 0;
+            
+            while (pokePower >= distance)
+            {
+                pokePower -= distance;
+                counter++;
+
+                if (pokePower == halfPower && exhaustionFactor != 0)
+                {
+                    pokePower /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(counter);
+        }
+    }
+}
