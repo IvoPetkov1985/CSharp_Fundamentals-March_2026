6eec5039e748ec5d39e284deec1c69e29d480874
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-15 11:34:58 +0200

    Task 11 uploaded.
    BigInteger used.

---
+++ DataTypes-Exercise/11.Snowballs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+namespace _11.Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowballsCount = int.Parse(Console.ReadLine());
+            int highestSnow = 0;
+            int highestTime = 0;
+            int highestQuality = 0;
+            BigInteger highestValue = 0;
+
+            for (int ball = 0; ball < snowballsCount; ball++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger snowballValue = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+
+                if (snowballValue > highestValue)
+                {
+                    highestSnow = snowballSnow;
+                    highestTime = snowballTime;
+                    highestQuality = snowballQuality;
+                    highestValue = snowballValue;
+                }
+            }
+
+            Console.WriteLine($"{highestSnow} : {highestTime} = {highestValue} ({highestQuality})");
+        }
+    }
+}
