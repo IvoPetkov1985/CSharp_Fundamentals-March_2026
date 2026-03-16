7ab050883aecde475307590d71bcf5fcacefcc7d
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-16 13:45:36 +0200

    Tasks 1 and 2 uploaded.

---
+++ DataTypes-MoreExercise/01.DataTypeFinder.v2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01.DataTypeFinder.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string type = string.Empty;
+
+            while (input != "END")
+            {
+                if (int.TryParse(input, out int result))
+                {
+                    type = "integer";
+                }
+                else if (double.TryParse(input, out double number))
+                {
+                    type = "floating point";
+                }
+                else if (char.TryParse(input, out char c))
+                {
+                    type = "character";
+                }
+                else if (bool.TryParse(input, out bool b))
+                {
+                    type = "boolean";
+                }
+                else
+                {
+                    type = "string";
+                }
+
+                Console.WriteLine($"{input} is {type} type");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ DataTypes-MoreExercise/02.FromLeftToTheRight.v2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.FromLeftToTheRight.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                long[] numbers = Console.ReadLine()
+                    .Split(" ")
+                    .Select(long.Parse)
+                    .ToArray();
+
+                long leftNumber = numbers.First();
+                long rightNumber = numbers.Last();
+                long maxNumber = Math.Abs(Math.Max(leftNumber, rightNumber));
+
+                int sumDigits = 0;
+
+                while (maxNumber > 0)
+                {
+                    sumDigits += (int)(maxNumber % 10);
+                    maxNumber /= 10;
+                }
+
+                Console.WriteLine(sumDigits);
+            }
+        }
+    }
+}
