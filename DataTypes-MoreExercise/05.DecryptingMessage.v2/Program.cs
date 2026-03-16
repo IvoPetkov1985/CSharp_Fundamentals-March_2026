30909ccbf544305f70d5c8d3dc3e22c4bba82750
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-16 13:46:38 +0200

    Tasks 5 and 6 uploaded.
    .sln file added.

---
+++ DataTypes-MoreExercise/05.DecryptingMessage.v2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _05.DecryptingMessage.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte offset = byte.Parse(Console.ReadLine());
+            byte numberOfCharacters = byte.Parse(Console.ReadLine());
+
+            string output = string.Empty;
+
+            for (int i = 0; i < numberOfCharacters; i++)
+            {
+                char inputLetter = char.Parse(Console.ReadLine());
+                int letterCode = (int)inputLetter;
+                letterCode += offset;
+                output += (char)letterCode;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ DataTypes-MoreExercise/06.BalancedBrackets.v2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _06.BalancedBrackets.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            char opening = '\0';
+            int counterOpening = 0;
+            int counterClosing = 0;
+            bool areBalanced = true;
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "(" && opening != '(')
+                {
+                    opening = '(';
+                    counterOpening++;
+                }
+                else if ((line == "(" && opening == '(') || (line == ")" && opening == '\0'))
+                {
+                    areBalanced = false;
+                    break;
+                }
+                else if (line == ")" && opening == '(')
+                {
+                    counterClosing++;
+                    opening = '\0';
+                }
+            }
+
+            if (areBalanced && (counterOpening == counterClosing))
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
