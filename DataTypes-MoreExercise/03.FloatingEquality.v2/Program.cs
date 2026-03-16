babed4e846903d6caf364bb015041c5598e40feb
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-16 13:46:05 +0200

    Tasks 3 and 4 uploaded.

---
+++ DataTypes-MoreExercise/03.FloatingEquality.v2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.FloatingEquality.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double precision = 0.000001;
+
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double diff = Math.Abs(firstNumber - secondNumber);
+            bool areEqual = diff < precision;
+
+            Console.WriteLine(areEqual);
+        }
+    }
+}
---
+++ DataTypes-MoreExercise/04.RefactoringPrimeChecker.v2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _04.RefactoringPrimeChecker.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = 2; currentNumber <= maxNumber; currentNumber++)
+            {
+                bool isPrime = true;
+
+                for (int divisor = 2; divisor < currentNumber; divisor++)
+                {
+                    if (currentNumber % divisor == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                Console.WriteLine("{0} -> {1}", currentNumber, isPrime ? "true" : "false");
+            }
+        }
+    }
+}
