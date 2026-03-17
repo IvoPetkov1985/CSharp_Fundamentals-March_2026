c6b5022fe44e2da23f7cea48cb8bd60d32a78177
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-17 11:58:31 +0200

    Task 2 uploaded.
    Two solutions.

---
+++ Arrays-Lab/02.PrintNumbersInReverseOrder.v2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.PrintNumbersInReverseOrder.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[numbersCount];
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Reverse(numbers);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ Arrays-Lab/02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02.PrintNumbersInReverseOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCoumnt = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[numbersCoumnt];
+
+            for (int i = 0; i < numbersCoumnt; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                numbers[i] = currentNumber;
+            }
+
+            for (int i = numbers.Length - 1; i >= 1; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
