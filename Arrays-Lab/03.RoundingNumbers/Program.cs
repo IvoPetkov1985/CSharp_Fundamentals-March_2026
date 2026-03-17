ad8210d3abff93d46d7a819dfe885f36ded680c3
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-17 11:58:59 +0200

    Task 3 uploaded.

---
+++ Arrays-Lab/03.RoundingNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] realNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            int[] roundedNumbers = realNumbers
+                .Select(x => (int)Math.Round(x, MidpointRounding.AwayFromZero))
+                .ToArray();
+
+            for (int i = 0; i < realNumbers.Length; i++)
+            {
+                Console.WriteLine($"{realNumbers[i]} => {roundedNumbers[i]}");
+            }
+        }
+    }
+}
