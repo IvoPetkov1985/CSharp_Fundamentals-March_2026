efbcc8b72ccb0cf5672d6843e8a35726e476e9d3
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-17 11:59:28 +0200

    Task 4 uploaded.
    Two solutions.

---
+++ Arrays-Lab/04.ReverseArrayOfStrings.v2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _04.ReverseArrayOfStrings.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < inputArray.Length / 2; i++)
+            {
+                (inputArray[i], inputArray[inputArray.Length - 1 - i]) =
+                    (inputArray[inputArray.Length - 1 - i], inputArray[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", inputArray));
+        }
+    }
+}
---
+++ Arrays-Lab/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04.ReverseArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfStrings = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < arrayOfStrings.Length / 2; i++)
+            {
+                string temp = arrayOfStrings[i];
+                arrayOfStrings[i] = arrayOfStrings[arrayOfStrings.Length - 1 - i];
+                arrayOfStrings[arrayOfStrings.Length - 1 - i] = temp;
+            }
+
+            Console.WriteLine(string.Join(" ", arrayOfStrings));
+        }
+    }
+}
