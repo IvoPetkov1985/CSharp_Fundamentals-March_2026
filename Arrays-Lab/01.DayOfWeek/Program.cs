901b4b556f22c96d75c5ca5921199a9ff1131ca3
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-17 11:57:46 +0200

    Task 1 uploaded.

---
+++ Arrays-Lab/01.DayOfWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.DayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            if (inputNumber > 0 && inputNumber < 8)
+            {
+                Console.WriteLine(days[inputNumber - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
