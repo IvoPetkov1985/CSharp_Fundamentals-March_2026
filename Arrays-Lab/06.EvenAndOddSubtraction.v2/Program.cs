29be86147d6fd4986310c5f4075f82ac2d4afbe3
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-18 14:33:08 +0200

    Task 6 uploaded.

---
+++ Arrays-Lab/06.EvenAndOddSubtraction.v2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _06.EvenAndOddSubtraction.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int currentNumber = inputArray[i];
+
+                if (currentNumber % 2 == 0)
+                {
+                    evenSum += currentNumber;
+                }
+                else
+                {
+                    oddSum += currentNumber;
+                }
+            }
+
+            int result = evenSum - oddSum;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Arrays-Lab/06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06.EvenAndOddSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int result = inputArray.Where(x => x % 2 == 0).Sum() -
+                inputArray.Where(x => x % 2 != 0).Sum();
+
+            Console.WriteLine(result);
+        }
+    }
+}
