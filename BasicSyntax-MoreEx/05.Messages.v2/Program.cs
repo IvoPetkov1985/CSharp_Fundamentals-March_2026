7f950fa5661bf05e98c063fc08ce869afdf682fb
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-18 16:27:02 +0200

    Task 5 uploaded.
    Another, more interesting solution of Messages. .sln file added.

---
+++ BasicSyntax-MoreEx/05.Messages.v2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _05.Messages.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string message = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                string inputLine = Console.ReadLine();
+                int num = int.Parse(inputLine[0].ToString());
+                int length = inputLine.Length;
+                int multiplier = 3;
+                int aCode = 97;
+
+                char letter = (char)(aCode + ((num - 2) * multiplier) + (length - 1));
+
+                if (num >= 8)
+                {
+                    letter++;
+                }
+
+                if (num == 0)
+                {
+                    letter = ' ';
+                }
+
+                message += letter;
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
