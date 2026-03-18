c9161300f46e38e49a180bedb6da3fc56095592b
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-18 14:33:52 +0200

    Tasks 7 and 8 uploaded.
    .sln file added.

---
+++ Arrays-Lab/07.EqualArrays/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _07.EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] secondArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            bool areIdentical = true;
+            int sum = 0;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                {
+                    areIdentical = false;
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+
+                sum += firstArray[i];
+            }
+
+            if (areIdentical == true)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
