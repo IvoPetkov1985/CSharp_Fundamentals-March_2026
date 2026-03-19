f4688d4a193ae9feaccbf30fd052a4406ef130d7
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-19 10:27:57 +0200

    Tasks 3 and 4 uploaded.

---
+++ Arrays-Exercise/03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _03.Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            int[] firstArray = new int[inputNumber];
+            int[] secondArray = new int[inputNumber];
+
+            for (int i = 0; i < inputNumber; i++)
+            {
+                int[] inputArray = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (i % 2 == 0)
+                {
+                    firstArray[i] = inputArray.First();
+                    secondArray[i] = inputArray.Last();
+                }
+                else
+                {
+                    firstArray[i] = inputArray.Last();
+                    secondArray[i] = inputArray.First();
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", firstArray));
+            Console.WriteLine(string.Join(" ", secondArray));
+        }
+    }
+}
---
+++ Arrays-Exercise/04.ArrayRotation/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            int rotationsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotationsCount % inputArray.Length; i++)
+            {
+                int temp = inputArray[0];
+
+                for (int j = 0; j < inputArray.Length - 1; j++)
+                {
+                    inputArray[j] = inputArray[j + 1];
+                }
+
+                inputArray[^1] = temp;
+            }
+
+            Console.WriteLine(string.Join(" ", inputArray));
+        }
+    }
+}
