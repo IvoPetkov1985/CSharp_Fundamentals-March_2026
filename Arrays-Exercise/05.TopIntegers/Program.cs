b92617bda0b1532579f7215cce136a6c4cc0b486
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-19 10:28:22 +0200

    Task 5 uploaded.

---
+++ Arrays-Exercise/05.TopIntegers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _05.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] inputArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => long.Parse(x))
+                .ToArray();
+
+            List<long> topInts = new();
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                bool isTop = true;
+
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    if (inputArray[i] <= inputArray[j])
+                    {
+                        isTop = false;
+                    }
+                }
+
+                if (isTop == true)
+                {
+                    topInts.Add(inputArray[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", topInts));
+        }
+    }
+}
