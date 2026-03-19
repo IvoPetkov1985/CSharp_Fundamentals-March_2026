06a350366902804ec95540b479fc7c541a638b1f
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-19 10:27:26 +0200

    Task 2 uploaded.
    Two solutions.

---
+++ Arrays-Exercise/02.CommonElements.v2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.CommonElements.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] secondArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> result = new();
+
+            foreach (string el in secondArray)
+            {
+                foreach (string compared in firstArray)
+                {
+                    if (compared == el)
+                    {
+                        result.Add(compared);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
