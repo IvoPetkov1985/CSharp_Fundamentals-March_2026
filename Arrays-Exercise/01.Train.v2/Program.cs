deafbbc65bf72a5535d19e1daa0b42ab3b4f240f
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-19 10:26:48 +0200

    Task 1 uploaded.
    Two solutions.

---
+++ Arrays-Exercise/01.Train.v2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.Train.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+
+            int[] train = new int[wagons];
+
+            for (int i = 0; i < wagons; i++)
+            {
+                train[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(" ", train));
+            Console.WriteLine(train.Sum());
+        }
+    }
+}
---
+++ Arrays-Exercise/01.Train/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonsCount = int.Parse(Console.ReadLine());
+
+            int[] train = new int[wagonsCount];
+
+            for (int i = 0; i < wagonsCount; i++)
+            {
+                int passengers = int.Parse(Console.ReadLine());
+                train[i] = passengers;
+            }
+
+            int totalPassengers = 0;
+
+            for (int i = 0; i < train.Length - 1; i++)
+            {
+                totalPassengers += train[i];
+                Console.Write(train[i] + " ");
+            }
+
+            totalPassengers += train[^1];
+            Console.WriteLine(train[^1]);
+            Console.WriteLine(totalPassengers);
+        }
+    }
+}
