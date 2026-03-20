b0efbd4914b9e778f1923b40aacd9762dc476d01
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-20 10:56:47 +0200

    Tasks 7 and 8 uploaded.

---
+++ Arrays-Exercise/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _07.MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int counter = 1;
+            int bestCounter = 0;
+            int element = 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 1;
+                }
+
+                if (counter > bestCounter)
+                {
+                    bestCounter = counter;
+                    element = nums[i];
+                }
+            }
+
+            for (int j = 1; j <= bestCounter; j++)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+    }
+}
---
+++ Arrays-Exercise/08.MagicSum/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _08.MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            int magicSum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    if ((inputArray[i] + inputArray[j]) == magicSum)
+                    {
+                        Console.WriteLine($"{inputArray[i]} {inputArray[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
