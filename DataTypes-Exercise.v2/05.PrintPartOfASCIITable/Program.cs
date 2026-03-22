30a9f94d9929daff093a079b71ecca0a0dedadf1
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-22 11:57:51 +0200

    Tasks 4, 5 and 6 uploaded.

---
+++ DataTypes-Exercise.v2/05.PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _05.PrintPartOfASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstCharIndex = int.Parse(Console.ReadLine());
+            int secondCharIndex = int.Parse(Console.ReadLine());
+
+            char[] chars = new char[(secondCharIndex - firstCharIndex) + 1];
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = (char)(i + firstCharIndex);
+            }
+
+            Console.WriteLine(string.Join(" ", chars));
+        }
+    }
+}
---
+++ DataTypes-Exercise.v2/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _06.TriplesOfLatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            int initialCode = 97;
+
+            for (int i = 0; i < inputNumber; i++)
+            {
+                for (int j = 0; j < inputNumber; j++)
+                {
+                    for (int k = 0; k < inputNumber; k++)
+                    {
+                        char firstLetter = (char)(i + initialCode);
+                        char secondLetter = (char)(j + initialCode);
+                        char thirdLetter = (char)(k + initialCode);
+                        Console.WriteLine($"{firstLetter}{secondLetter}{thirdLetter}");
+                    }
+                }
+            }
+        }
+    }
+}
