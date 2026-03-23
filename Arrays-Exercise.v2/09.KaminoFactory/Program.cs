2666ebffdda501892d76a69ccca06357c163d89c
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-23 16:21:36 +0200

    Tasks 9 and 10 uploaded.
    .sln file added.

---
+++ Arrays-Exercise.v2/09.KaminoFactory/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace _09.KaminoFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dnaLength = int.Parse(Console.ReadLine());
+
+            int[] bestSequence = new int[dnaLength];
+            int bestCounter = 0;
+            int bestIndex = int.MaxValue;
+            int bestNumber = 0;
+            int bestSum = 0;
+
+            int dnaNumber = 0;
+
+            while (true)
+            {
+                string dnaSequenceInput = Console.ReadLine();
+
+                if (dnaSequenceInput == "Clone them!")
+                {
+                    break;
+                }
+
+                int[] dnaSequence = dnaSequenceInput
+                    .Split("!", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                dnaNumber++;
+                int startIndex = -1;
+                int counter = 0;
+                int sequenceBestCounter = 0;
+
+                for (int i = 0; i < dnaLength; i++)
+                {
+                    if (dnaSequence[i] == 1)
+                    {
+                        counter++;
+
+                        if (counter > sequenceBestCounter)
+                        {
+                            sequenceBestCounter = counter;
+                            startIndex = i - (counter - 1);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+
+                int dnaSum = dnaSequence.Sum();
+                bool isBetter = false;
+
+                if (sequenceBestCounter > bestCounter)
+                {
+                    isBetter = true;
+                }
+                else if (sequenceBestCounter == bestCounter)
+                {
+                    if (startIndex < bestIndex)
+                    {
+                        isBetter = true;
+                    }
+                    else if (startIndex == bestIndex)
+                    {
+                        if (dnaSum > bestSum)
+                        {
+                            isBetter = true;
+                        }
+                    }
+                }
+
+                if (isBetter == true || dnaNumber == 1)
+                {
+                    bestSequence = dnaSequence;
+                    bestNumber = dnaNumber;
+                    bestSum = dnaSum;
+                    bestCounter = sequenceBestCounter;
+                    bestIndex = startIndex;
+                }
+            }
+
+            Console.WriteLine($"Best DNA sample {bestNumber} with sum: {bestSum}.");
+            Console.WriteLine(string.Join(" ", bestSequence));
+        }
+    }
+}
---
+++ Arrays-Exercise.v2/10.LadyBugs/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _10.LadyBugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+
+            int[] ladybugsIndices = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] field = new int[fieldSize];
+
+            for (int i = 0; i < ladybugsIndices.Length; i++)
+            {
+                int ladybugIndex = ladybugsIndices[i];
+
+                if (ladybugIndex >= 0 && ladybugIndex < fieldSize)
+                {
+                    field[ladybugIndex] = 1;
+                }
+            }
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "end")
+            {
+                string[] tokens = inputLine.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int index = int.Parse(tokens[0]);
+                string direction = tokens[1];
+                int distance = int.Parse(tokens[2]);
+
+                if (index >= 0 && index < fieldSize && field[index] == 1)
+                {
+                    field[index] = 0;
+
+                    if (direction == "right")
+                    {
+                        do
+                        {
+                            index += distance;
+                        }
+                        while (index >= 0 && index < fieldSize && field[index] == 1);
+
+                        if (index >= 0 && index < fieldSize)
+                        {
+                            field[index] = 1;
+                        }
+                    }
+                    else if (direction == "left")
+                    {
+                        do
+                        {
+                            index -= distance;
+                        }
+                        while (index >= 0 && index < fieldSize && field[index] == 1);
+
+                        if (index >= 0 && index < fieldSize)
+                        {
+                            field[index] = 1;
+                        }
+                    }
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
