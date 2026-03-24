b7188671da23bba7ce2a44aeb376e5a4fd9e43ad
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-24 11:25:18 +0200

    Tasks 5 and 6 uploaded.

---
+++ Methods-Lab/05.Orders/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string productName = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            Calculate(productName, quantity);
+        }
+
+        static void Calculate(string product, int quantity)
+        {
+            Dictionary<string, double> priceList = new()
+            {
+                { "coffee", 1.50 },
+                { "water", 1.00 },
+                { "coke", 1.40 },
+                { "snacks", 2.00 }
+            };
+
+            double sum = priceList[product] * quantity;
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
---
+++ Methods-Lab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _06.CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int sideB = int.Parse(Console.ReadLine());
+            int area = GetArea(sideA, sideB);
+            Console.WriteLine(area);
+        }
+
+        static int GetArea(int sideA, int sideB)
+        {
+            int area = sideA * sideB;
+            return area;
+        }
+    }
+}
