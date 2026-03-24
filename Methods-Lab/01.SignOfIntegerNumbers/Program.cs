30617f946e709fa2714a4e8f1235c4dd146c5737
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-24 11:24:06 +0200

    Tasks 1 and 2 uploaded.

---
+++ Methods-Lab/01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01.SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintSign(number);
+        }
+
+        static void PrintSign(int number)
+        {
+            string result = string.Empty;
+
+            if (number > 0)
+            {
+                result = $"The number {number} is positive.";
+            }
+            else if (number < 0)
+            {
+                result = $"The number {number} is negative.";
+            }
+            else if (number == 0)
+            {
+                result = $"The number {number} is zero.";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Methods-Lab/02.Grades/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace _02.Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            PrintGradeInWords(grade);
+        }
+
+        static void PrintGradeInWords(double grade)
+        {
+            string gradeInWords = string.Empty;
+
+            if (grade >= 2.00 && grade < 3.00)
+            {
+                gradeInWords = "Fail";
+            }
+            else if (grade >= 3.00 && grade < 3.50)
+            {
+                gradeInWords = "Poor";
+            }
+            else if (grade >= 3.50 && grade < 4.50)
+            {
+                gradeInWords = "Good";
+            }
+            else if (grade >= 4.50 && grade < 5.50)
+            {
+                gradeInWords = "Very good";
+            }
+            else if (grade >= 5.50 && grade <= 6.00)
+            {
+                gradeInWords = "Excellent";
+            }
+
+            Console.WriteLine(gradeInWords);
+        }
+    }
+}
