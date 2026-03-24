5f01c38a155d78acf4ef93b5fff4f876efb8535e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-24 11:24:49 +0200

    Tasks 3 and 4 uploaded.

---
+++ Methods-Lab/03.Calculations/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _03.Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string calculation = Console.ReadLine();
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (calculation == "add")
+            {
+                Add(firstNumber, secondNumber);
+            }
+            else if (calculation == "subtract")
+            {
+                Subtract(firstNumber, secondNumber);
+            }
+            else if (calculation == "multiply")
+            {
+                Multiply(firstNumber, secondNumber);
+            }
+            else if (calculation == "divide")
+            {
+                Divide(firstNumber, secondNumber);
+            }
+        }
+
+        static void Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine(result);
+        }
+
+        static void Subtract(int num1, int num2)
+        {
+            int result = num1 - num2;
+            Console.WriteLine(result);
+        }
+
+        static void Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            Console.WriteLine(result);
+        }
+
+        static void Divide(int num1, int num2)
+        {
+            int result = num1 / num2;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Methods-Lab/04.PrintingTriangle/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _04.PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintFirstHalf(number);
+            PrintSecondHalf(number);
+        }
+
+        static void PrintSecondHalf(int number)
+        {
+            for (int i = number - 1; i >= 1; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+
+        static void PrintFirstHalf(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                PrintLine(1, i);
+            }
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
