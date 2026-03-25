4260ada1d19ddcf648cc48aedda4eb69330afeb0
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-25 13:28:02 +0200

    Tasks 9 and 10 uploaded.

---
+++ Methods-Lab/09.GreaterOfTwoValues/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _09.GreaterOfTwoValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+
+            switch (dataType)
+            {
+                case "int":
+                    int firstNum = int.Parse(Console.ReadLine());
+                    int secondNum = int.Parse(Console.ReadLine());
+                    Console.WriteLine(GetMax(firstNum, secondNum));
+                    break;
+                case "char":
+                    char firstChar = char.Parse(Console.ReadLine());
+                    char secondChar = char.Parse(Console.ReadLine());
+                    Console.WriteLine(GetMax(firstChar, secondChar));
+                    break;
+                case "string":
+                    string firstString = Console.ReadLine();
+                    string secondString = Console.ReadLine();
+                    Console.WriteLine(GetMax(firstString, secondString));
+                    break;
+            }
+        }
+
+        static int GetMax(int firstValue, int secondValue)
+        {
+            return Math.Max(firstValue, secondValue);
+        }
+
+        static char GetMax(char firstValue, char secondValue)
+        {
+            if (firstValue > secondValue)
+            {
+                return firstValue;
+            }
+
+            return secondValue;
+        }
+
+        static string GetMax(string firstValue, string secondValue)
+        {
+            if (firstValue.CompareTo(secondValue) == 1)
+            {
+                return firstValue;
+            }
+
+            return secondValue;
+        }
+    }
+}
---
+++ Methods-Lab/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _10.MultiplyEvensByOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+        }
+
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            int firstMultiplyer = GetSumOfEvenDigits(number);
+            int secondMultiplyer = GetSumOfOddDigits(number);
+            return firstMultiplyer * secondMultiplyer;
+        }
+
+        static int GetSumOfEvenDigits(int number)
+        {
+            int copyNumber = number;
+            int evenSum = 0;
+
+            while (copyNumber > 0)
+            {
+                int digit = copyNumber % 10;
+
+                if (digit % 2 == 0)
+                {
+                    evenSum += digit;
+                }
+
+                copyNumber /= 10;
+            }
+
+            return evenSum;
+        }
+
+        static int GetSumOfOddDigits(int number)
+        {
+            int copyNumber = number;
+            int oddSum = 0;
+
+            while (copyNumber > 0)
+            {
+                int digit = copyNumber % 10;
+
+                if (digit % 2 != 0)
+                {
+                    oddSum += digit;
+                }
+
+                copyNumber /= 10;
+            }
+
+            return oddSum;
+        }
+    }
+}
