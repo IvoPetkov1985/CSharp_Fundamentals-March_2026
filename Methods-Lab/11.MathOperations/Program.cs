495d8843fb416714ac86c5d3f58e720e335539a9
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-25 13:29:09 +0200

    Task 11 uploaded.
    .sln file added.

---
+++ Methods-Lab/11.MathOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _11.MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            string operand = Console.ReadLine();
+            int secondNumber = int.Parse(Console.ReadLine());
+            double result = Calculate(firstNumber, operand, secondNumber);
+            Console.WriteLine(result);
+        }
+
+        static double Calculate(int x1, string operand, int x2)
+        {
+            switch (operand)
+            {
+                case "+": return x1 + x2;
+                case "-": return x1 - x2;
+                case "*": return x1 * x2;
+                case "/": return x1 / (double)x2;
+            }
+
+            return 0;
+        }
+    }
+}
