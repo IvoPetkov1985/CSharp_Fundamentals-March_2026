50144e830409ed857b978b65cc71e6843b094d3e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-25 13:27:29 +0200

    Tasks 7 and 8 uploaded.

---
+++ Methods-Lab/07.RepeatString/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _07.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine(Concatenate(text, count));
+        }
+
+        static string Concatenate(string text, int count)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                result += text;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Methods-Lab/08.MathPower/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalculatePower(baseNumber, power));
+        }
+
+        static double CalculatePower(double number, int power)
+        {
+            double result = 1;
+
+            for (int i = 1; i <= power; i++)
+            {
+                result *= number;
+            }
+
+            return result;
+        }
+    }
+}
