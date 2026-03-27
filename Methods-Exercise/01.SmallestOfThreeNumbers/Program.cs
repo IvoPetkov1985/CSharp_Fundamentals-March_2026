345ecc12181df0e5442ba8d6e1cfc550f4da98da
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-27 12:21:10 +0200

    Tasks 1 and 2 uploaded.

---
+++ Methods-Exercise/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSmallestNumber(firstNumber, secondNumber, thirdNumber));
+        }
+
+        static int GetSmallestNumber(int num1, int num2, int num3)
+        {
+            int[] numbers = new int[3] { num1, num2, num3 };
+            int minNumber = numbers.Min();
+            return minNumber;
+        }
+    }
+}
---
+++ Methods-Exercise/02.VowelsCount/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _02.VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            int vowelsCount = GetVowelsCount(inputText);
+            Console.WriteLine(vowelsCount);
+        }
+
+        static int GetVowelsCount(string text)
+        {
+            int counter = 0;
+            char[] vowels = new char[12] { 'a', 'o', 'u', 'e', 'i', 'y', 'A', 'O', 'U', 'E', 'I', 'Y' };
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentLetter = text[i];
+
+                if (vowels.Contains(currentLetter))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
