550405b459eee4792dc626795c375fe69b1434ed
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-27 12:21:54 +0200

    Tasks 3 and 4 uploaded.

---
+++ Methods-Exercise/03.CharactersInRange/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _03.CharactersInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            char minChar = GetCharWithMinValue(firstChar, secondChar);
+            char maxChar = '\0';
+
+            if (minChar == firstChar)
+            {
+                maxChar = secondChar;
+            }
+            else
+            {
+                maxChar = firstChar;
+            }
+
+            string result = GetLineOfChars(minChar, maxChar);
+            Console.WriteLine(result.TrimEnd());
+        }
+
+        static char GetCharWithMinValue(char first, char second)
+        {
+            char minValueChar = (char)Math.Min(first, second);
+            return minValueChar;
+        }
+
+        static string GetLineOfChars(char start, char end)
+        {
+            string line = string.Empty;
+
+            for (int i = (int)start + 1; i < (int)end; i++)
+            {
+                line += $"{(char)i} ";
+            }
+
+            return line;
+        }
+    }
+}
---
+++ Methods-Exercise/04.PasswordValidator/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _04.PasswordValidator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            bool isPasswordValid = true;
+
+            if (IsLengthValid(password) == false)
+            {
+                isPasswordValid = false;
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+
+            if (AreAllSymbolsValid(password) == false)
+            {
+                isPasswordValid = false;
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+
+            if (IsDigitCountValid(password) == false)
+            {
+                isPasswordValid = false;
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+
+            if (isPasswordValid == true)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+
+        static bool IsLengthValid(string password)
+        {
+            if (password.Length >= 6 && password.Length <= 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool AreAllSymbolsValid(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (!char.IsDigit(password[i]) && !char.IsLetter(password[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool IsDigitCountValid(string password)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsDigit(password[i]))
+                {
+                    counter++;
+                }
+            }
+
+            return counter >= 2;
+        }
+    }
+}
