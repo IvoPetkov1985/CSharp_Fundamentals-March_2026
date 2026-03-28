031f35f1be036d55c3ca09a8e8608b1a8d554ffe
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-28 13:41:21 +0200

    Tasks 9 and 10 uploaded.

---
+++ Methods-Exercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _09.PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                Console.WriteLine(IsPalindrome(input));
+            }
+        }
+
+        static string IsPalindrome(string input)
+        {
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    return "false";
+                }
+            }
+
+            return "true";
+        }
+    }
+}
---
+++ Methods-Exercise/10.TopNumber/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _10.TopNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int endValue = int.Parse(Console.ReadLine());
+            int startValue = 1;
+
+            for (int i = startValue; i <= endValue; i++)
+            {
+                int currentNumber = i;
+
+                if (IsSumDigitsDivisibleBy8(currentNumber) 
+                    && ContainsOddDigit(currentNumber))
+                {
+                    Console.WriteLine(currentNumber);
+                }
+            }
+        }
+
+        static bool IsSumDigitsDivisibleBy8(int number)
+        {
+            int sumDigits = 0;
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sumDigits += digit;
+                number /= 10;
+            }
+
+            return sumDigits % 8 == 0;
+        }
+
+        static bool ContainsOddDigit(int number)
+        {
+            while (number > 0)
+            {
+                int digit = number % 10;
+
+                if (digit % 2 != 0)
+                {
+                    return true;
+                }
+
+                number /= 10;
+            }
+
+            return false;
+        }
+    }
+}
