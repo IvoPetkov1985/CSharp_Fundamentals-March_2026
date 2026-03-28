732e5d2b548d023afc967d721df9fbedd534fd34
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-28 13:40:47 +0200

    Tasks 7 and 8 uploaded.

---
+++ Methods-Exercise/07.NxNMatrix/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07.NxNMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputNumber; i++)
+            {
+                Console.WriteLine(CreateLine(inputNumber));
+            }
+        }
+
+        static string CreateLine(int number)
+        {
+            int[] numbers = new int[number];
+            
+            for (int i = 0; i < number; i++)
+            {
+                numbers[i] = number;
+            }
+
+            return string.Join(" ", numbers);
+        }
+    }
+}
---
+++ Methods-Exercise/08.FactorialDivision/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _08.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            long firstFactorial = CalculateFactorial(firstNumber);
+            long secondFactorial = CalculateFactorial(secondNumber);
+            Console.WriteLine(DivideFactorials(firstFactorial, secondFactorial));
+        }
+
+        static long CalculateFactorial(int number)
+        {
+            long result = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        static string DivideFactorials(long first, long second)
+        {
+            double result = first / (double)second;
+            return $"{result:F2}";
+        }
+    }
+}
