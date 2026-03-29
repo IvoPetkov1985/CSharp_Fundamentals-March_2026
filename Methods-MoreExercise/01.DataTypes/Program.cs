1aecf5915324cb515e07b3a66af4bc09c9469138
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-29 18:44:10 +0300

    Tasks 1 and 2 uploaded.

---
+++ Methods-MoreExercise/01.DataTypes/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _01.DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+            string value = Console.ReadLine();
+
+            if (dataType == "int")
+            {
+                int intNumber = int.Parse(value);
+                ProcessData(intNumber);
+            }
+            else if (dataType == "real")
+            {
+                double realNumber = double.Parse(value);
+                ProcessData(realNumber);
+            }
+            else if (dataType == "string")
+            {
+                ProcessData(value);
+            }
+        }
+
+        static void ProcessData(int number)
+        {
+            int result = number * 2;
+            Console.WriteLine(result);
+        }
+
+        static void ProcessData(double number)
+        {
+            double result = number * 1.5;
+            Console.WriteLine($"{result:F2}");
+        }
+
+        static void ProcessData(string text)
+        {
+            string line = $"${text}$";
+            Console.WriteLine(line);
+        }
+    }
+}
---
+++ Methods-MoreExercise/02.CenterPoint/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.CenterPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstPointX = double.Parse(Console.ReadLine());
+            double firstPointY = double.Parse(Console.ReadLine());
+            double secondPointX = double.Parse(Console.ReadLine());
+            double secondPointY = double.Parse(Console.ReadLine());
+
+            double firstDistance = GetPointDistance(firstPointX, firstPointY);
+            double secondDistance = GetPointDistance(secondPointX, secondPointY);
+
+            if (firstDistance < secondDistance)
+            {
+                Console.WriteLine($"({firstPointX}, {firstPointY})");
+            }
+            else if (firstDistance > secondDistance)
+            {
+                Console.WriteLine($"({secondPointX}, {secondPointY})");
+            }
+            else
+            {
+                Console.WriteLine($"({firstPointX}, {firstPointY})");
+            }
+        }
+
+        static double GetPointDistance(double x, double y)
+        {
+            double result = Math.Sqrt(Math.Abs(x * x) + Math.Abs(y * y));
+            return result;
+        }
+    }
+}
