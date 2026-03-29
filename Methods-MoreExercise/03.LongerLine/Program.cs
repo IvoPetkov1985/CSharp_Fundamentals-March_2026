f6199d551fda99d5aa049be65e921510613b837e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-29 18:45:18 +0300

    Tasks 3 and 4 uploaded.

---
+++ Methods-MoreExercise/03.LongerLine/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _03.LongerLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+
+            double firstLength = GetLength(x1, y1, x2, y2);
+            double secondLength = GetLength(x3, y3, x4, y4);
+
+            if (firstLength >= secondLength)
+            {
+                PrintResult(x1, y1, x2, y2);
+            }
+            else
+            {
+                PrintResult(x3, y3, x4, y4);
+            }
+        }
+
+        static double GetLength(double x1, double y1, double x2, double y2)
+        {
+            double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return distance;
+        }
+
+        static double GetDistanceToCenter(double x, double y)
+        {
+            double distance = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            return distance;
+        }
+
+        static void PrintResult(double x1, double y1, double x2, double y2)
+        {
+            if (GetDistanceToCenter(x1, y1) <= GetDistanceToCenter(x2, y2))
+            {
+                Console.WriteLine($"({x1}, {y1})({x2}, {y2})");
+            }
+            else
+            {
+                Console.WriteLine($"({x2}, {y2})({x1}, {y1})");
+            }
+        }
+    }
+}
---
+++ Methods-MoreExercise/04.TribonacciSequence/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _04.TribonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            List<int> numbers = new() { 1, 1, 2 };
+
+            for (int i = 3; i < length; i++)
+            {
+                numbers.Add(numbers[i - 1] + numbers[i - 2] + numbers[i - 3]);
+            }
+
+            if (length <= 3)
+            {
+                Console.WriteLine(string.Join(" ", numbers.Take(length)));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }                
+        }
+    }
+}
