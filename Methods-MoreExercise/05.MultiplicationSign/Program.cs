1a1ffb31f80d23c603ffeecb7aef30c62d02c30a
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-29 18:45:57 +0300

    Task 5 uploaded.
    sln file added.

---
+++ Methods-MoreExercise/05.MultiplicationSign/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05.MultiplicationSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int[] numbers = new int[3] { firstNumber, secondNumber, thirdNumber };
+            Console.WriteLine(FindProductSign(numbers));
+        }
+
+        static string FindProductSign(int[] ints)
+        {
+            if (ints.Contains(0))
+            {
+                return "zero";
+            }
+
+            int[] negativeNumbers = ints.Where(x => x < 0).ToArray();
+
+            if (negativeNumbers.Length % 2 != 0)
+            {
+                return "negative";
+            }
+            else
+            {
+                return "positive";
+            }
+        }
+    }
+}
