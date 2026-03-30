95741f5114735e962baa9765249167df87442554
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-30 09:55:12 +0300

    Task 4 fancy solve.
    Nested loop used, dynamically updating an existing array.
    sln file added.

---
+++ Methods-MoreExercise/04.TribonacciSequence.v3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _04.TribonacciSequence.v3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            int[] array = { 1, 1, 2 };
+
+            for (int i = 4; i <= numbersCount; i++)
+            {
+                int[] currentArray = new int[i];
+
+                for (int j = 0; j < currentArray.Length - 1; j++)
+                {
+                    currentArray[j] = array[j];                   
+                }
+
+                currentArray[currentArray.Length - 1] = array.TakeLast(3).Sum();
+                array = currentArray;
+            }
+
+            Console.WriteLine(string.Join(" ", array.Take(numbersCount)));
+        }
+    }
+}
