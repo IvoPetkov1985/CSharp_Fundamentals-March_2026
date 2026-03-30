dfef68c7e7a392236a2dcbf7efc97080adee1779
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-30 11:28:13 +0300

    Tasks 1 and 2 from More Ex uploaded.

---
+++ Arrays-Exercise.v2/01.EncryptSortAndPrintArray/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01.EncryptSortAndPrintArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                string text = Console.ReadLine();
+                int sum = 0;
+
+                foreach (char symbol in text)
+                {
+                    if (symbol == 'A' || symbol == 'a' ||
+                        symbol == 'O' || symbol == 'o' ||
+                        symbol == 'U' || symbol == 'u' ||
+                        symbol == 'E' || symbol == 'e' ||
+                        symbol == 'I' || symbol == 'i')
+                    {
+                        sum += (int)symbol * text.Length;
+                    }
+                    else
+                    {
+                        sum += (int)symbol / text.Length;
+                    }
+                }
+
+                numbers[i] = sum;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, numbers.OrderBy(x => x)));
+        }
+    }
+}
---
+++ Arrays-Exercise.v2/02.PascalTriangle/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _02.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCount = int.Parse(Console.ReadLine());
+            long[] prevRow = Array.Empty<long>();
+
+            for (int count = 1; count <= rowsCount; count++)
+            {
+                long[] row = new long[count];
+                row[0] = 1;
+                row[^1] = 1;
+
+                for (int i = 1; i < row.Length - 1; i++)
+                {
+                    row[i] = prevRow[i - 1] + prevRow[i];
+                }
+
+                Console.WriteLine(string.Join(" ", row));
+
+                prevRow = row;
+            }
+        }
+    }
+}
