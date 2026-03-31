8f68d9dc299b27f71aebb148e369c1e4e2d08ac9
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-31 13:53:39 +0300

    Tasks 1 and 2 uploaded.
    Gauss' Trick with two solutions.

---
+++ Lists-Lab/02.GaussTrick.v2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02.GaussTrick.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int count = numbers.Count;
+
+            for (int i = 0; i < count / 2; i++)
+            {
+                int firstValue = numbers[i];
+                int lastValue = numbers[^1];
+                numbers[i] = firstValue + lastValue;
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ Lists-Lab/02.GaussTrick/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.GaussTrick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputArray = Console.ReadLine()
+                .Split(" ")
+                .Select(double.Parse)
+                .ToArray();
+
+            List<double> resultList = new();
+
+            for (int i = 0; i < inputArray.Length / 2; i++)
+            {
+                double sum = inputArray[i] + inputArray[inputArray.Length - 1 - i];
+                resultList.Add(sum);
+            }
+
+            if (inputArray.Length % 2 != 0)
+            {
+                resultList.Add(inputArray[inputArray.Length / 2]);
+            }
+
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+    }
+}
