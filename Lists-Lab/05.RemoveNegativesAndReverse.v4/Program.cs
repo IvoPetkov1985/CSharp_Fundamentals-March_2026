31992726965e06098a48326d71691774223ae9c8
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-03 12:06:22 +0300

    Another solution of task 5 added.

---
+++ Lists-Lab/05.RemoveNegativesAndReverse.v4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.RemoveNegativesAndReverse.v4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] positiveNumbers = Array.FindAll(inputNumbers, x => x >= 0);
+            Array.Reverse(positiveNumbers);
+            
+            if (positiveNumbers.Length > 0)
+            {
+                Console.WriteLine(string.Join(" ", positiveNumbers));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
