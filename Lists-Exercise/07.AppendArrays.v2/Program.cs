787073ee8246ee9d98934dd116de25c57db16db7
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-04 13:54:18 +0300

    Task 7 uploaded.
    Two different solutions - lists and arrays.

---
+++ Lists-Exercise/07.AppendArrays.v2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _07.AppendArrays.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrays = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries);
+
+            Array.Reverse(arrays);
+            string result = string.Empty;
+
+            foreach (string arrayAsStr in arrays)
+            {
+                int[] currentArray = arrayAsStr
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                foreach (int number in currentArray)
+                {
+                    result += $"{number} ";
+                }
+            }
+
+            result = result.TrimEnd();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Lists-Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            List<string> arrays = inputLine
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            arrays.Reverse();
+
+            List<int> resultList = new();
+
+            foreach (string line in arrays)
+            {
+                int[] numbers = line
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                foreach (int number in numbers)
+                {
+                    resultList.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+    }
+}
