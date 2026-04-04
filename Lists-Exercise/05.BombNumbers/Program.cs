eb03d0edc51ce9b2f3a8230a1ab6ce497808b041
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-04 13:53:23 +0300

    Tasks 5 and 6 uploaded.

---
+++ Lists-Exercise/05.BombNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _05.BombNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] details = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int bombNumber = details[0];
+            int power = details[1];
+
+            while (numbers.IndexOf(bombNumber) != -1)
+            {
+                int startIndex = numbers.IndexOf(bombNumber) - power;
+                if (startIndex < 0)
+                {
+                    startIndex = 0;
+                }
+
+                int endIndex = numbers.IndexOf(bombNumber) + power;
+                if (endIndex >= numbers.Count)
+                {
+                    endIndex = numbers.Count - 1;
+                }
+
+                numbers.RemoveRange(startIndex, endIndex - startIndex + 1);
+            }
+
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ Lists-Exercise/06.CardsGame/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _06.CardsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstDeck = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondDeck = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            while (firstDeck.Count > 0 && secondDeck.Count > 0)
+            {
+                int firstPlayerCard = firstDeck[0];
+                int secondPlayerCard = secondDeck[0];
+                firstDeck.RemoveAt(0);
+                secondDeck.RemoveAt(0);
+
+                if (firstPlayerCard > secondPlayerCard)
+                {
+                    firstDeck.Add(firstPlayerCard);
+                    firstDeck.Add(secondPlayerCard);
+                }
+                else if (firstPlayerCard < secondPlayerCard)
+                {
+                    secondDeck.Add(secondPlayerCard);
+                    secondDeck.Add(firstPlayerCard);
+                }
+            }
+
+            if (firstDeck.Count > 0)
+            {
+                Console.WriteLine($"First player wins! Sum: {firstDeck.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Second player wins! Sum: {secondDeck.Sum()}");
+            }
+        }
+    }
+}
