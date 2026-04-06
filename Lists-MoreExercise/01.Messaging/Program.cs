35d53753828e9b8c8cae6ce7ce31a5cf9f82515f
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-06 09:54:47 +0300

    Tasks 1 and 2 uploaded.

---
+++ Lists-MoreExercise/01.Messaging/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01.Messaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string text = Console.ReadLine();
+            string message = string.Empty;
+
+            foreach (int number in numbers)
+            {
+                int sumDigits = 0;
+                int numberCopy = number;
+
+                while (numberCopy > 0)
+                {
+                    sumDigits += numberCopy % 10;
+                    numberCopy /= 10;
+                }
+
+                int index = sumDigits % text.Length;
+                message += text[index];
+                text = text.Remove(index, 1);
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Lists-MoreExercise/02.CarRace/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _02.CarRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int middleIndex = numbers.Length / 2;
+            float firstCarTime = 0;
+
+            for (int i = 0; i < middleIndex; i++)
+            {
+                int time = numbers[i];
+
+                if (time != 0)
+                {
+                    firstCarTime += time;
+                }
+                else
+                {
+                    firstCarTime *= 0.8f;
+                }
+            }
+
+            float secondCarTime = 0;
+
+            for (int i = numbers.Length - 1; i > middleIndex; i--)
+            {
+                int time = numbers[i];
+
+                if (time != 0)
+                {
+                    secondCarTime += time;
+                }
+                else
+                {
+                    secondCarTime *= 0.8f;
+                }
+            }
+
+            if (firstCarTime < secondCarTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {firstCarTime}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {secondCarTime}");
+            }
+        }
+    }
+}
