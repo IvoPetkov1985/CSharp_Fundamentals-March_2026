1ba87e55c8b593ef5cdee42a2f8a70ed685f16d1
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-06 09:55:33 +0300

    Tasks 3 and 4 uploaded.

---
+++ Lists-MoreExercise/03.TakeSkipRope/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _03.TakeSkipRope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+
+            List<int> digits = new();
+            List<char> nonDigits = new();
+
+            foreach (char symbol in inputText)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    int digit = int.Parse(symbol.ToString());
+                    digits.Add(digit);
+                }
+                else
+                {
+                    nonDigits.Add(symbol);
+                }
+            }
+
+            List<int> takeList = new();
+            List<int> skipList = new();
+
+            for (int i = 0; i < digits.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    takeList.Add(digits[i]);
+                }
+                else
+                {
+                    skipList.Add(digits[i]);
+                }
+            }
+
+            List<char> resultList = new();
+            int index = 0;
+
+            for (int i = 0; i < takeList.Count; i++)
+            {
+                int take = takeList[i];
+                int skip = skipList[i];
+                List<char> temp = nonDigits.Skip(index).Take(take).ToList();
+                resultList.AddRange(temp);
+                index += take + skip;
+            }
+
+            Console.WriteLine(string.Join("", resultList));
+        }
+    }
+}
---
+++ Lists-MoreExercise/04.MixedUpLists/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _04.MixedUpLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] secondArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int minLength = Math.Min(firstArray.Length, secondArray.Length);
+            List<int> resultList = new();
+            Array.Reverse(secondArray);
+
+            for (int i = 0; i < minLength; i++)
+            {
+                resultList.Add(firstArray[i]);
+                resultList.Add(secondArray[i]);
+            }
+
+            int[] rangeNumbers = new int[2];
+
+            if (firstArray.Length > secondArray.Length)
+            {
+                rangeNumbers = firstArray.TakeLast(2).ToArray();
+            }
+            else
+            {
+                rangeNumbers = secondArray.TakeLast(2).ToArray();
+            }
+
+            int minValue = rangeNumbers.Min();
+            int maxValue = rangeNumbers.Max();
+            resultList = resultList.FindAll(x => x > minValue && x < maxValue);
+            resultList.Sort();
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+    }
+}
