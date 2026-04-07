72bda1e39a625d328888c72928c9fc2bcd00a404
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-07 11:07:21 +0300

    Task 5 uploaded.
    .sln file added.

---
+++ Lists-MoreExercise.v2/05.DrumSet/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _05.DrumSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+
+            List<int> initialQualities = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> currentQualities = new(initialQualities);
+
+            string command = Console.ReadLine();
+
+            while (command != "Hit it again, Gabsy!")
+            {
+                int hitPower = int.Parse(command);
+                currentQualities = currentQualities
+                    .Select(x => x -= hitPower).ToList();
+
+                for (int i = 0; i < currentQualities.Count; i++)
+                {
+                    int drumQuality = currentQualities[i];
+
+                    if (drumQuality <= 0)
+                    {
+                        decimal drumPrice = initialQualities[i] * 3;
+
+                        if (drumPrice <= budget)
+                        {
+                            budget -= drumPrice;
+                            currentQualities[i] = initialQualities[i];
+                        }
+                        else
+                        {
+                            currentQualities.RemoveAt(i);
+                            initialQualities.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", currentQualities));
+            Console.WriteLine($"Gabsy has {budget:F2}lv.");
+        }
+    }
+}
