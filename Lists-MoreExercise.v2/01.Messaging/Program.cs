21b20481bc199968be851293ae1a24f78812350e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-07 11:05:52 +0300

    Tasks 1 and 2 uploaded.

---
+++ Lists-MoreExercise.v2/01.Messaging/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01.Messaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string inputText = Console.ReadLine();
+            string message = string.Empty;
+
+            foreach (int currentNumber in numbers)
+            {
+                int sum = 0;
+                int copyNumber = currentNumber;
+
+                while (copyNumber > 0)
+                {
+                    int digit = copyNumber % 10;
+                    sum += digit;
+                    copyNumber /= 10;
+                }
+
+                int index = sum % inputText.Length;
+                char symbol = inputText[index];
+                message += symbol;
+                inputText = inputText.Remove(index, 1);
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Lists-MoreExercise.v2/02.CarRace/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _02.CarRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] trace = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            float leftCarTime = GetFirstCarTime(trace);
+            float rightCarTime = GetSecondCarTime(trace);
+
+            if (leftCarTime <= rightCarTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftCarTime}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {rightCarTime}");
+            }
+        }
+
+        static float GetFirstCarTime(int[] trace)
+        {
+            float time = 0;
+
+            for (int i = 0; i < trace.Length / 2; i++)
+            {
+                if (trace[i] != 0)
+                {
+                    time += trace[i];
+                }
+                else
+                {
+                    time *= 0.8f;
+                }
+            }
+
+            return time;
+        }
+
+        static float GetSecondCarTime(int[] trace)
+        {
+            float time = 0;
+
+            for (int i = trace.Length - 1; i > trace.Length / 2; i--)
+            {
+                if (trace[i] != 0)
+                {
+                    time += trace[i];
+                }
+                else
+                {
+                    time *= 0.8f;
+                }
+            }
+
+            return time;
+        }
+    }
+}
