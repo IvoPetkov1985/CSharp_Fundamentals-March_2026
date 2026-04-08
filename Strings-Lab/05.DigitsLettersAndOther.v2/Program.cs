87cf26f373624a5e91771c3489858dcacfdf299e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-08 12:26:06 +0300

    Task 5 uploaded.
    Two different solutions. sln file added.

---
+++ Strings-Lab/05.DigitsLettersAndOther.v2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _05.DigitsLettersAndOther.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            char[] allSymbols = inputLine.ToCharArray();
+            char[] digits = Array.FindAll(allSymbols, c => char.IsDigit(c));
+            char[] letters = Array.FindAll(allSymbols, c => char.IsLetter(c));
+            char[] other = Array.FindAll(allSymbols, c => !char.IsLetterOrDigit(c));
+            Console.WriteLine(string.Join(string.Empty, digits));
+            Console.WriteLine(string.Join(string.Empty, letters));
+            Console.WriteLine(string.Join(string.Empty, other));
+        }
+    }
+}
---
+++ Strings-Lab/05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05.DigitsLettersAndOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            string digits = string.Empty;
+            string letters = string.Empty;
+            string others = string.Empty;
+
+            foreach (char c in inputLine)
+            {
+                if (c >= 48 && c <= 57)
+                {
+                    digits += c;
+                }
+                else if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+                {
+                    letters += c;
+                }
+                else
+                {
+                    others += c;
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
