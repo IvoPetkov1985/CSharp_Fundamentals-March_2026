8268b1189de10768863e984d78f57a03b120e9d6
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-10 10:50:17 +0300

    Task 3 uploaded.
    Two solutions.

---
+++ Strings-Exercise/03.ExtractFile.v2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.ExtractFile.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+
+            string fileInfo = inputLine.Split('\\').Last();
+            string[] fileTokens = fileInfo.Split('.');
+            string fileName = fileTokens[0];
+            string extensionName = fileTokens[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extensionName}");
+        }
+    }
+}
---
+++ Strings-Exercise/03.ExtractFile/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+
+            int fileNameIndex = filePath.LastIndexOf('\\');
+            int fileExtensionIndex = filePath.LastIndexOf('.');
+
+            string fileName = filePath.Substring(fileNameIndex + 1, fileExtensionIndex - fileNameIndex - 1);
+            string extension = filePath.Substring(fileExtensionIndex + 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
