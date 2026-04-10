48257e33b26a17a3a90d163491b1e535c1af8263
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-10 10:49:14 +0300

    Tasks 1 and 2 uploaded.

---
+++ Strings-Exercise/01.ValidUsernames/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string username in usernames)
+            {
+                if (IsLengthValid(username) && AreAllCharactersValid(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+
+        static bool IsLengthValid(string username)
+        {
+            if (username.Length > 3 && username.Length < 16)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool AreAllCharactersValid(string username)
+        {
+            if (username.All(c => char.IsLetterOrDigit(c) || c == '_' || c == '-'))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Strings-Exercise/02.CharacterMultiplier/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string firstStr = strings[0];
+            string secondStr = strings[1];
+
+            int maxLength = Math.Max(firstStr.Length, secondStr.Length);
+            int totalSum = 0;
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < firstStr.Length && i < secondStr.Length)
+                {
+                    totalSum += firstStr[i] * secondStr[i];
+                }
+                else if (i < firstStr.Length)
+                {
+                    totalSum += firstStr[i];
+                }
+                else if (i < secondStr.Length)
+                {
+                    totalSum += secondStr[i];
+                }
+            }
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
