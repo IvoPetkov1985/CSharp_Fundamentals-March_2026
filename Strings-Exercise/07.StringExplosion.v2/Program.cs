5cccc04636ece7484cacd11908077f7458fb8342
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-11 13:50:00 +0300

    Task 7 uploaded.
    String Explosion with two solutions.

---
+++ Strings-Exercise/07.StringExplosion.v2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace _07.StringExplosion.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder result = new();
+
+            int strength = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '>')
+                {
+                    int newStrength = int.Parse(text[i + 1].ToString());
+                    result.Append(text[i]);
+                    strength += newStrength;
+                }
+                else if (strength == 0)
+                {
+                    result.Append(text[i]);
+                }
+                else if (strength > 0)
+                {
+                    strength--;
+                }
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ Strings-Exercise/07.StringExplosion/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            int strength = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '>')
+                {
+                    int number = int.Parse(text[i + 1].ToString());
+                    strength += number;
+                }
+                else if (strength > 0 && text[i] != '>')
+                {
+                    text = text.Remove(i, 1);
+                    strength--;
+                    i--;
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
