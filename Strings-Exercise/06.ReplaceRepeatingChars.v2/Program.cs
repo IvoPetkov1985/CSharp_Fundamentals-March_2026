40eaeb42c682bd4c5e06323cc4d8ecac565cc3c2
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-11 13:49:10 +0300

    Task 6 uploaded.
    Replace Repeating Chars with two solutions.

---
+++ Strings-Exercise/06.ReplaceRepeatingChars.v2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace _06.ReplaceRepeatingChars.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder builder = new();
+
+            char current = text[0];
+            builder.Append(current);
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (text[i] == current)
+                {
+                    continue;
+                }
+                else
+                {
+                    current = text[i];
+                    builder.Append(text[i]);
+                }
+            }
+
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
---
+++ Strings-Exercise/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char current = text[0];
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                char c = text[i];
+
+                if (c == current)
+                {
+                    text = text.Remove(i, 1);
+                    i--;
+                }
+                else
+                {
+                    current = c;
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
