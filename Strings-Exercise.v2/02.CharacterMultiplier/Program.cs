a5fe698ce53c65cf9739e19cb72ee4a6382d628c
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-12 19:40:26 +0300

    Tasks 2 and 3 uploaded.

---
+++ Strings-Exercise.v2/02.CharacterMultiplier/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02.CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string firstString = strings.First();
+            string secondString = strings.Last();
+
+            int maxLength = Math.Max(firstString.Length, secondString.Length);
+            int sum = 0;
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                int firstMultiplier = i < firstString.Length ? firstString[i] : 1;
+                int secondMultiplier = i < secondString.Length ? secondString[i] : 1;
+                sum += firstMultiplier * secondMultiplier;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Strings-Exercise.v2/03.ExtractFile/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+
+            int startIndex = filePath.LastIndexOf(@"\") + 1;
+            string fileInfo = filePath.Substring(startIndex);
+
+            int extensionIndex = fileInfo.LastIndexOf(".") + 1;
+            string fileName = fileInfo.Substring(0, extensionIndex - 1);
+            string extensionName = fileInfo.Substring(extensionIndex);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extensionName}");
+        }
+    }
+}
