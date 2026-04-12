74c37b55b2a174e20fa269f08f4b4ccdb2fab3b8
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-12 19:40:57 +0300

    Task 4 uploaded.

---
+++ Strings-Exercise.v2/04.CaesarCipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace _04.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            StringBuilder builder = new();
+
+            foreach (char symbol in inputText)
+            {
+                char encrypredSymbol = (char)(symbol + 3);
+                builder.Append(encrypredSymbol);
+            }
+
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
