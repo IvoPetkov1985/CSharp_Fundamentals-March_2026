41c265583bc31b1d96e514c722caa8693e62149f
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-12 19:39:37 +0300

    Task 1 uploaded.
    Valid Usernames with two solutions.

---
+++ Strings-Exercise.v2/01.ValidUsernames.v2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.ValidUsernames.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string username in usernames)
+            {
+                bool isLengthValid = false;
+                bool areCharsValid = false;
+
+                if (username.Length >= 3 && username.Length <= 16)
+                {
+                    isLengthValid = true;
+                }
+
+                if (username.All(c => char.IsLetterOrDigit(c) || c == '_' || c == '-'))
+                {
+                    areCharsValid = true;
+                }
+
+                if (isLengthValid && areCharsValid)
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+    }
+}
---
+++ Strings-Exercise.v2/01.ValidUsernames/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ");
+
+            foreach (string name in usernames)
+            {
+                if (IsLengthValid(name) && AreAllCharctersValid(name))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+
+        static bool IsLengthValid(string username)
+        {
+            if (username.Length < 3 || username.Length > 16)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static bool AreAllCharctersValid(string username)
+        {
+            Regex pattern = new(@"^[A-Za-z0-9_-]*$");
+
+            if (pattern.IsMatch(username))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
