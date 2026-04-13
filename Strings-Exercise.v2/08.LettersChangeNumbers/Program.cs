85e7048fc69eb379442f67ef06ef6a51fbb32cb2
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-13 14:02:39 +0300

    Task 8 uploaded.
    And .sln file added.

---
+++ Strings-Exercise.v2/08.LettersChangeNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _08.LettersChangeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            double totalSum = 0;
+
+            foreach (string text in input)
+            {
+                char firstLetter = text.First();
+                char secondLetter = text.Last();
+                double number = double.Parse(text.Substring(1, text.Length - 2));
+                int firstLetterPosition = GetLetterPosition(firstLetter);
+                int secondLetterPosition = GetLetterPosition(secondLetter);
+
+                if (char.IsUpper(firstLetter))
+                {
+                    number /= firstLetterPosition;
+                }
+                else
+                {
+                    number *= firstLetterPosition;
+                }
+
+                if (char.IsUpper(secondLetter))
+                {
+                    number -= secondLetterPosition;
+                }
+                else
+                {
+                    number += secondLetterPosition;
+                }
+
+                totalSum += number;
+            }
+
+            Console.WriteLine($"{totalSum:F2}");
+        }
+
+        static int GetLetterPosition(char letter)
+        {
+            return letter.ToString().ToLower().First() - 96;
+        }
+    }
+}
