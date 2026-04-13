2753008968d2856010cc8f372128cfa5ebd1eaf6
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-13 14:01:55 +0300

    Tasks 6 and 7 uploaded.

---
+++ Strings-Exercise.v2/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            StringBuilder output = new();
+
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                output.Append(inputText[i]);
+
+                while (i < inputText.Length - 1 && inputText[i] == inputText[i + 1])
+                {
+                    i++;
+                }
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
---
+++ Strings-Exercise.v2/07.StringExplosion/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = string.Empty;
+
+            int strength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += input[i + 1] - '0';
+                    output += input[i];
+                }
+                else if (strength > 0)
+                {
+                    strength--;
+                }
+                else if (strength == 0)
+                {
+                    output += input[i];
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
