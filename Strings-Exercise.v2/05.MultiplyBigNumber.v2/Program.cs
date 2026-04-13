e86d1ff99e2ce62e1695f1e934afc0affe0b4e8b
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-13 14:01:19 +0300

    Task 5 uploaded.
    Multiply Big Number with two solutions.

---
+++ Strings-Exercise.v2/05.MultiplyBigNumber.v2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05.MultiplyBigNumber.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNumber = Console.ReadLine();
+            int multiplyer = int.Parse(Console.ReadLine());
+
+            if (bigNumber == null || bigNumber == "0" || multiplyer == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            int[] productNumber = new int[bigNumber.Length + 1];
+
+            for (int i = bigNumber.Length - 1; i >= 0; i--)
+            {
+                int product = multiplyer * (bigNumber[i] - '0');
+                productNumber[i + 1] += product;
+
+                if (productNumber[i + 1] >= 10)
+                {
+                    productNumber[i] += productNumber[i + 1] / 10;
+                    productNumber[i + 1] %= 10;
+                }
+            }
+
+            Console.WriteLine(string.Join(string.Empty, productNumber).TrimStart('0'));
+        }
+    }
+}
---
+++ Strings-Exercise.v2/05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05.MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNumber = Console.ReadLine();
+            int multiplyer = int.Parse(Console.ReadLine());
+
+            if (bigNumber == "0" || multiplyer == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            int[] productNumber = new int[bigNumber.Length + 1];
+            int carry = 0;
+
+            for (int i = bigNumber.Length - 1; i >= 0; i--)
+            {
+                int digit = bigNumber[i] - '0';
+                int product = digit * multiplyer + carry;
+                productNumber[i + 1] = product % 10;
+                carry = product / 10;
+            }
+
+            productNumber[0] = carry;
+            Console.WriteLine(string.Join(string.Empty, productNumber).TrimStart('0'));
+        }
+    }
+}
