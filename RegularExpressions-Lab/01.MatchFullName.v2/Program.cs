227016be60f1b603f57a64fc1870fed72bac780c
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-14 12:16:22 +0300

    Task 1 uploaded.
    Match Full Name with two solutions.

---
+++ RegularExpressions-Lab/01.MatchFullName.v2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex pattern = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+            MatchCollection matches = pattern.Matches(input);
+            IEnumerable<string> validNames = matches.Select(m => m.Value);
+            Console.WriteLine(string.Join(" ", validNames));
+        }
+    }
+}
---
+++ RegularExpressions-Lab/01.MatchFullName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string allNames = Console.ReadLine();
+            string namePattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            List<string> validNames = new();
+
+            MatchCollection namesCollection = Regex.Matches(allNames, namePattern);
+
+            foreach (Match name in namesCollection)
+            {
+                validNames.Add(name.Value);
+            }
+
+            Console.WriteLine(string.Join(" ", validNames));
+        }
+    }
+}
