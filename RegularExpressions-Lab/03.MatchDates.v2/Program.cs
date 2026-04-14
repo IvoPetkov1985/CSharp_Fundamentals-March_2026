67c2adb7d5b32afd54ef1722df073c4ce1c21748
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-14 12:18:16 +0300

    Task 3 uploaded.
    Match Dates with two solutions. sln file added.

---
+++ RegularExpressions-Lab/03.MatchDates.v2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03.MatchDates.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex datePattern = new Regex(@"\b(?<day>\d{2})([/\-\.])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b");
+            MatchCollection matches = datePattern.Matches(input);
+
+            foreach (Match date in matches)
+            {
+                string day = date.Groups["day"].Value;
+                string month = date.Groups["month"].Value;
+                string year = date.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
---
+++ RegularExpressions-Lab/03.MatchDates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string allDates = Console.ReadLine();
+            string datePattern = @"\b(?<day>\d{2})([/\-\.])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            MatchCollection collection = Regex.Matches(allDates, datePattern);
+
+            foreach (Match date in collection)
+            {
+                int day = int.Parse(date.Groups["day"].Value);
+                string month = date.Groups["month"].Value;
+                int year = int.Parse(date.Groups["year"].Value);
+
+                Console.WriteLine($"Day: {day:D2}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
