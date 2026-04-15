3e26a59b572264de9ec8ab99cd388d2150111944
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-15 10:00:21 +0300

    Task 4 uploaded.
    Second solution. sln file added.

---
+++ AssocArrays-Lab/04.WordFilter.v2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _04.WordFilter.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] filteredWords = Array.FindAll(words, w => w.Length % 2 == 0);
+            Console.WriteLine(string.Join(Environment.NewLine, filteredWords));
+        }
+    }
+}
