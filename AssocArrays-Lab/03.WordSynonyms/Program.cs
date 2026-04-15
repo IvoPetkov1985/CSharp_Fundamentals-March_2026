3f4921a47f1fa1ab94beb6cbfbedd5d723d098b4
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-15 09:59:42 +0300

    Tasks 3 and 4 uploaded.

---
+++ AssocArrays-Lab/03.WordSynonyms/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wordsCount = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> words = new();
+
+            for (int i = 0; i < wordsCount; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (words.ContainsKey(word) == false)
+                {
+                    words.Add(word, new List<string>());
+                }
+
+                words[word].Add(synonym);
+            }
+
+            foreach (KeyValuePair<string, List<string>> kvp in words)
+            {
+                Console.WriteLine($"{kvp.Key} - {string.Join(", ", kvp.Value)}");
+            }
+        }
+    }
+}
---
+++ AssocArrays-Lab/04.WordFilter/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _04.WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => w.Length % 2 == 0)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
