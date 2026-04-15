9ec5073a1461cc6c7c90b960760d95ad044f7ee0
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-15 09:58:43 +0300

    Tasks 1 and 2 uploaded.

---
+++ AssocArrays-Lab/01.CountRealNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> countNumbers = new();
+
+            foreach (double number in numbers)
+            {
+                if (!countNumbers.ContainsKey(number))
+                {
+                    countNumbers.Add(number, 0);
+                }
+
+                countNumbers[number]++;
+            }
+
+            foreach (KeyValuePair<double, int> kvp in countNumbers)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ AssocArrays-Lab/02.OddOccurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> occurrences = new();
+
+            foreach (string word in words)
+            {
+                string wordToLower = word.ToLower();
+
+                if (!occurrences.ContainsKey(wordToLower))
+                {
+                    occurrences[wordToLower] = 0;
+                }
+
+                occurrences[wordToLower]++;
+            }
+
+            List<string> selectedWords = new();
+
+            foreach (KeyValuePair<string, int> kvp in occurrences)
+            {
+                if (kvp.Value % 2 != 0)
+                {
+                    selectedWords.Add(kvp.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", selectedWords));
+        }
+    }
+}
