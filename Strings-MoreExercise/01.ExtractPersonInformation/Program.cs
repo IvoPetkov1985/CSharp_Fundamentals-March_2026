3b1d7d18ddea176edc9cca3aa9e283d5dd527d63
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-16 17:10:36 +0300

    Task 1 uploaded.

---
+++ Strings-MoreExercise/01.ExtractPersonInformation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace _01.ExtractPersonInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+            StringBuilder result = new();
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                string inputLine = Console.ReadLine();
+
+                int nameStartIndex = inputLine.IndexOf('@') + 1;
+                int nameEndIndex = inputLine.IndexOf('|');
+                string name = inputLine.Substring(nameStartIndex, nameEndIndex - nameStartIndex);
+
+                int ageStartIndex = inputLine.IndexOf('#') + 1;
+                int ageEndIndex = inputLine.IndexOf('*');
+                string age = inputLine.Substring(ageStartIndex, ageEndIndex - ageStartIndex);
+
+                result.AppendLine($"{name} is {age} years old.");
+            }
+
+            Console.WriteLine(result.ToString().TrimEnd());
+        }
+    }
+}
