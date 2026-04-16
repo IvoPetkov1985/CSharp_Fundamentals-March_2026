965ac088fe42f21631771644c6376912f46dfd70
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-16 17:11:14 +0300

    Tasks 2 and 3 uploaded.

---
+++ Strings-MoreExercise/02.ASCIISumator/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _02.ASCIISumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            char[] inputArray = input.ToCharArray();
+            char[] sortedChars = GetCharsSorted(firstChar, secondChar);
+            char smaller = sortedChars.First();
+            char bigger = sortedChars.Last();
+            char[] selectedChars = Array.FindAll(inputArray, c => c > smaller && c < bigger);
+            int sumChars = GetCharsSum(selectedChars);
+
+            Console.WriteLine(sumChars);
+        }
+
+        static char[] GetCharsSorted(char char1, char char2)
+        {
+            char[] chars = { char1, char2 };
+            Array.Sort(chars);
+            return chars;
+        }
+
+        static int GetCharsSum(char[] chars)
+        {
+            int sum = 0;
+
+            foreach (char c in chars)
+            {
+                sum += c;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Strings-MoreExercise/03.TreasureFinder/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _03.TreasureFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] keys = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string encrypredMessage = Console.ReadLine();
+
+            while (encrypredMessage != "find")
+            {
+                string decrypredMessage = string.Empty;
+
+                int index = 0;
+
+                for (int i = 0; i < encrypredMessage.Length; i++)
+                {
+                    int key = keys[index];
+                    char currentChar = encrypredMessage[i];
+                    char newChar = (char)(currentChar - key);
+                    decrypredMessage += newChar;
+                    index++;
+
+                    if (index == keys.Length)
+                    {
+                        index = 0;
+                    }
+                }
+
+                int treasureStartIndex = decrypredMessage.IndexOf('&') + 1;
+                int treasureEndIndex = decrypredMessage.LastIndexOf('&');
+                string treasure = decrypredMessage.Substring(treasureStartIndex, treasureEndIndex - treasureStartIndex);
+
+                int coordinatesStartIndex = decrypredMessage.IndexOf('<') + 1;
+                int coordinatesEndIndex = decrypredMessage.IndexOf('>');
+                string coordinates = decrypredMessage.Substring(coordinatesStartIndex, coordinatesEndIndex - coordinatesStartIndex);
+                Console.WriteLine($"Found {treasure} at {coordinates}");
+
+                encrypredMessage = Console.ReadLine();
+            }
+        }
+    }
+}
