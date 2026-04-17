ab6145003440db7df1b16ac8d46fbb91d28bee5b
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-17 14:22:11 +0300

    Task 1 uploaded.

---
+++ RegularExpressions-Exercise/01.Furniture/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _01.Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            Regex regex = new Regex(@">>(?<product>[A-Za-z]+)<<(?<price>\d+(\.\d+)?)!(?<quantity>\d+)");
+            List<string> boughtFurniture = new();
+            decimal totalSum = 0;
+
+            while (inputLine != "Purchase")
+            {
+                Match match = regex.Match(inputLine);
+
+                if (match.Success)
+                {
+                    string productName = match.Groups["product"].Value;
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+                    boughtFurniture.Add(productName);
+                    totalSum += price * quantity;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            StringBuilder builder = new();
+
+            builder.AppendLine("Bought furniture:");
+
+            foreach (string furniture in boughtFurniture)
+            {
+                builder.AppendLine(furniture);
+            }
+
+            builder.AppendLine($"Total money spend: {totalSum:F2}");
+            Console.WriteLine(builder.ToString().TrimEnd());
+        }
+    }
+}
