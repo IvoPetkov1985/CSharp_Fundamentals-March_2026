1905d134cdabe7fe3f7d45ecdb8496e97306cbcb
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-17 14:22:52 +0300

    Tasks 2 and 3 uploaded.

---
+++ RegularExpressions-Exercise/02.Race/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] participants = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string inputLine = Console.ReadLine();
+
+            Regex letterPattern = new(@"[A-Za-z]+");
+            Regex digitPattern = new(@"\d");
+            Dictionary<string, int> race = new();
+
+            while (inputLine != "end of race")
+            {
+                MatchCollection lettersCollection = letterPattern.Matches(inputLine);
+                string name = string.Empty;
+
+                foreach (Match letters in lettersCollection)
+                {
+                    name += letters.Value;
+                }
+
+                int points = 0;
+
+                if (participants.Contains(name))
+                {
+                    MatchCollection digits = digitPattern.Matches(inputLine);
+
+                    foreach (Match digit in digits)
+                    {
+                        int number = int.Parse(digit.Value);
+                        points += number;
+                    }
+
+                    if (!race.ContainsKey(name))
+                    {
+                        race.Add(name, 0);
+                    }
+
+                    race[name] += points;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            int counter = 0;
+            
+            foreach (var kvp in race.OrderByDescending(x => x.Value))
+            {
+                counter++;
+                if (counter == 1)
+                    Console.WriteLine($"1st place: {kvp.Key}");
+                else if (counter == 2)
+                    Console.WriteLine($"2nd place: {kvp.Key}");
+                else if (counter == 3)
+                    Console.WriteLine($"3rd place: {kvp.Key}");
+
+                if (counter == 4)
+                    break;
+            }
+        }
+    }
+}
---
+++ RegularExpressions-Exercise/03.SoftUniBarIncome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03.SoftUniBarIncome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new(@"%(?<name>[A-Z][a-z]+)%[^\|\$\%\.]*?<(?<product>\w+)>[^\|\$\%\.]*?\|(?<count>\d+)\|[^\|\$\%\.]*?(?<price>\d+(\.\d+)?)\$");
+            string inputLine = Console.ReadLine();
+            decimal totalIncome = 0;
+
+            while (inputLine != "end of shift")
+            {
+                Match match = pattern.Match(inputLine);
+
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+                    string product = match.Groups["product"].Value;
+                    int count = int.Parse(match.Groups["count"].Value);
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+                    decimal productPrice = price * count;
+                    totalIncome += productPrice;
+
+                    Console.WriteLine($"{name}: {product} - {productPrice:F2}");
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:F2}");
+        }
+    }
+}
