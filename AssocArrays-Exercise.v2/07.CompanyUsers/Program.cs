53ad150b9d2b8732681c8696fe3cdd113904e1d0
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-18 16:10:30 +0300

    Task 7 uploaded.
    .sln file added.

---
+++ AssocArrays-Exercise.v2/07.CompanyUsers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _07.CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, HashSet<string>> companiesMap = new();
+
+            while (true)
+            {
+                string inputLine = Console.ReadLine();
+
+                if (inputLine == "End")
+                {
+                    break;
+                }
+
+                string[] arguments = inputLine.Split(" -> ");
+                string companyName = arguments[0];
+                string employeeId = arguments[1];
+
+                companiesMap.TryAdd(companyName, new HashSet<string>());
+                companiesMap[companyName].Add(employeeId);
+            }
+
+            foreach (KeyValuePair<string, HashSet<string>> comapnyInfo in companiesMap)
+            {
+                Console.WriteLine(comapnyInfo.Key);
+
+                foreach (string id in comapnyInfo.Value)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+}
