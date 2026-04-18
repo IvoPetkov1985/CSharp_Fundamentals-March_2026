d6536d892a764ca4850057e5faba9d9f6525459f
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-18 16:09:11 +0300

    Tasks 3 and 4 uploaded

---
+++ AssocArrays-Exercise.v2/03.Orders/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _03.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> quantitiesMap = new();
+            Dictionary<string, decimal> pricesMap = new();
+
+            while (true)
+            {
+                string inputLine = Console.ReadLine();
+
+                if (inputLine == "buy")
+                {
+                    break;
+                }
+
+                string[] arguments = inputLine.Split();
+                string product = arguments[0];
+                decimal price = decimal.Parse(arguments[1]);
+                int quantity = int.Parse(arguments[2]);
+
+                if (!quantitiesMap.ContainsKey(product))
+                {
+                    quantitiesMap.Add(product, quantity);
+                    pricesMap.Add(product, price);
+                }
+                else
+                {
+                    quantitiesMap[product] += quantity;
+                    pricesMap[product] = price;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> productInfo in quantitiesMap)
+            {
+                decimal totalPrice = productInfo.Value * pricesMap[productInfo.Key];
+                Console.WriteLine($"{productInfo.Key} -> {totalPrice:F2}");
+            }
+        }
+    }
+}
---
+++ AssocArrays-Exercise.v2/04.SoftUniParking/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _04.SoftUniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int commandsCount = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> parkingLot = new();
+
+            for (int i = 0; i < commandsCount; i++)
+            {
+                string inputLine = Console.ReadLine();
+                string[] arguments = inputLine.Split();
+                string command = arguments[0];
+                string username = arguments[1];
+
+                if (command == "register")
+                {
+                    string licensePlateNumber = arguments[2];
+                    bool isRegistered = parkingLot.TryAdd(username, licensePlateNumber);
+
+                    if (isRegistered)
+                    {
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {parkingLot[username]}");
+                    }
+                }
+                else if (command == "unregister")
+                {
+                    bool isRemoved = parkingLot.Remove(username);
+
+                    if (isRemoved)
+                    {
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<string, string> carInfo in parkingLot)
+            {
+                Console.WriteLine($"{carInfo.Key} => {carInfo.Value}");
+            }
+        }
+    }
+}
