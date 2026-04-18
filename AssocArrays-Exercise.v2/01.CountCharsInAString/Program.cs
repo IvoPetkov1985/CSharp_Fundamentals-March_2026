1e7bb6065905848cbf5cb20296f6d081339036b4
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-18 16:08:16 +0300

    Tasks 1 and 2 uploaded.

---
+++ AssocArrays-Exercise.v2/01.CountCharsInAString/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+
+            Dictionary<char, int> charsCount = new();
+
+            foreach (char letter in inputLine)
+            {
+                if (letter != ' ')
+                {
+                    if (!charsCount.ContainsKey(letter))
+                    {
+                        charsCount.Add(letter, 0);
+                    }
+
+                    charsCount[letter]++;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> letterInfo in charsCount)
+            {
+                Console.WriteLine($"{letterInfo.Key} -> {letterInfo.Value}");
+            }
+        }
+    }
+}
---
+++ AssocArrays-Exercise.v2/02.AMinerTask/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _02.AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resourcesMap = new();
+
+            while (true)
+            {
+                string resource = Console.ReadLine();
+
+                if (resource == "stop")
+                {
+                    break;
+                }
+
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resourcesMap.ContainsKey(resource))
+                {
+                    resourcesMap.Add(resource, quantity);
+                }
+                else
+                {
+                    resourcesMap[resource] += quantity;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> resourceInfo in resourcesMap)
+            {
+                Console.WriteLine($"{resourceInfo.Key} -> {resourceInfo.Value}");
+            }
+        }
+    }
+}
