c8b0ee5e9c27d1e81757c2be7ea10c78bb39b99c
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-18 16:09:43 +0300

    Tasks 5 and 6 uploaded.

---
+++ AssocArrays-Exercise.v2/05.Courses/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _05._Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> academy = new();
+
+            while (true)
+            {
+                string inputLine = Console.ReadLine();
+
+                if (inputLine == "end")
+                {
+                    break;
+                }
+
+                string[] arguments = inputLine.Split(" : ");
+                string courseName = arguments[0];
+                string studentName = arguments[1];
+
+                academy.TryAdd(courseName, new List<string>());
+                academy[courseName].Add(studentName);
+            }
+
+            foreach (KeyValuePair<string, List<string>> courseInfo in academy)
+            {
+                Console.WriteLine($"{courseInfo.Key}: {courseInfo.Value.Count}");
+
+                foreach (string studentName in courseInfo.Value)
+                {
+                    Console.WriteLine($"-- {studentName}");
+                }
+            }
+        }
+    }
+}
---
+++ AssocArrays-Exercise.v2/06.StudentAcademy/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _06.StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCount = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentsData = new();
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                studentsData.TryAdd(studentName, new List<double>());
+                studentsData[studentName].Add(grade);
+            }
+
+            foreach (KeyValuePair<string, List<double>> student in studentsData)
+            {
+                double averageGrade = student.Value.Average();
+
+                if (averageGrade >= 4.5)
+                {
+                    Console.WriteLine($"{student.Key} -> {averageGrade:F2}");
+                }
+            }
+        }
+    }
+}
