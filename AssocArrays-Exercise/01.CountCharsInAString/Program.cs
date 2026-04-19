6244cdd0b8d37f08d2ea2be242d50b22a8df2279
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-19 11:28:52 +0300

    Tasks 1 and 2 uploaded.

---
+++ AssocArrays-Exercise/01.CountCharsInAString/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> occurrences = new();
+
+            foreach (char letter in text)
+            {
+                if (letter == ' ')
+                {
+                    continue;
+                }
+
+                if (!occurrences.ContainsKey(letter))
+                {
+                    occurrences.Add(letter, 0);
+                }
+
+                occurrences[letter]++;
+            }
+
+            foreach (KeyValuePair<char, int> kvp in occurrences)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ AssocArrays-Exercise/02.AMinerTask/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new();
+            string inputResource = Console.ReadLine();
+
+            while (inputResource != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(inputResource))
+                {
+                    resources.Add(inputResource, 0);
+                }
+
+                resources[inputResource] += quantity;
+                inputResource = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, int> kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
