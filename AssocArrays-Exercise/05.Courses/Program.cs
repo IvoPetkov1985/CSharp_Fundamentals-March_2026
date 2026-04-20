d91ffebf6359a3ff37edfddd7a61ffbc97af578e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-20 16:44:55 +0300

    Tasks 5 and 6 uploaded.

---
+++ AssocArrays-Exercise/05.Courses/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _05.Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            Dictionary<string, List<string>> courses = new();
+
+            while (inputLine != "end")
+            {
+                string[] tokens = inputLine.Split(" : ");
+                string courseName = tokens[0];
+                string studentName = tokens[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses.Add(courseName, new List<string>());
+                }
+
+                courses[courseName].Add(studentName);
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, List<string>> courseInfo in courses)
+            {
+                Console.WriteLine($"{courseInfo.Key}: {courseInfo.Value.Count}");
+
+                foreach (string studentName in courseInfo.Value)
+                {
+                    Console.WriteLine($"-- {studentName}");
+                }
+            }
+        }
+    }
+}
---
+++ AssocArrays-Exercise/06.StudentAcademy/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _06.StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCount = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> academy = new();
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!academy.ContainsKey(studentName))
+                {
+                    academy.Add(studentName, new List<double>());
+                }
+
+                academy[studentName].Add(grade);
+            }
+
+            foreach (KeyValuePair<string, List<double>> studentInfo in academy)
+            {
+                string studentName = studentInfo.Key;
+                double averageGrade = studentInfo.Value.Average();
+
+                if (averageGrade >= 4.50)
+                {
+                    Console.WriteLine($"{studentName} -> {averageGrade:F2}");
+                }
+            }
+        }
+    }
+}
