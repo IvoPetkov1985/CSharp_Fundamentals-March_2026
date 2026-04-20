bc716de1b669f4ebb8b39857b39c308690c56a14
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-20 16:45:37 +0300

    Task 7 uploaded.
    Company Users with two solutions.

---
+++ AssocArrays-Exercise/07.CompanyUsers.v2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07.CompanyUsers.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, HashSet<string>> agenda = new();
+
+            string inputLine = null;
+            while ((inputLine = Console.ReadLine()) != "End")
+            {
+                string[] tokens = inputLine.Split(" -> ");
+                string companyName = tokens[0];
+                string userId = tokens[1];
+
+                if (!agenda.ContainsKey(companyName))
+                {
+                    agenda.Add(companyName, new HashSet<string>());
+                }
+
+                agenda[companyName].Add(userId);
+            }
+
+            foreach (KeyValuePair<string, HashSet<string>> companyInfo in agenda)
+            {
+                Console.WriteLine(companyInfo.Key);
+
+                foreach (string id in companyInfo.Value)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+}
---
+++ AssocArrays-Exercise/07.CompanyUsers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace _07.CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new();
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "End")
+            {
+                string[] tokens = inputLine.Split(" -> ");
+                string companyName = tokens[0];
+                string employeeId = tokens[1];
+
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies[companyName] = new List<string>();
+                }
+
+                if (!companies[companyName].Contains(employeeId))
+                {
+                    companies[companyName].Add(employeeId);
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            StringBuilder builder = new();
+
+            foreach (KeyValuePair<string, List<string>> companyInfo in companies)
+            {
+                builder.AppendLine(companyInfo.Key);
+
+                foreach (string id in companyInfo.Value)
+                {
+                    builder.AppendLine($"-- {id}");
+                }
+            }
+
+            Console.WriteLine(builder.ToString().TrimEnd());
+        }
+    }
+}
