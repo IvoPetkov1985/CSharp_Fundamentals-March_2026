833357b815963482d0e65d38b884d3309e24fdea
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-21 10:56:16 +0300

    Task 1 uploaded.
    Counter Strike with two solutions.

---
+++ ExamPreparation/01.CounterStrike.v2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _01.CounterStrike.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            int wonBattles = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End of battle")
+                {
+                    Console.WriteLine($"Won battles: {wonBattles}. Energy left: {energy}");
+                    break;
+                }
+
+                int distance = int.Parse(input);
+
+                if (distance > energy)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wonBattles} won battles and {energy} energy");
+                    break;
+                }
+
+                energy -= distance;
+                wonBattles++;
+
+                if (wonBattles % 3 == 0)
+                {
+                    energy += wonBattles;
+                }
+            }
+        }
+    }
+}
---
+++ ExamPreparation/01.CounterStrike/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _01.CounterStrike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            int battlesWon = 0;
+
+            while (command != "End of battle")
+            {
+                int distance = int.Parse(command);
+
+                if (energy >= distance)
+                {
+                    battlesWon++;
+                    energy -= distance;
+
+                    if (battlesWon % 3 == 0)
+                    {
+                        energy += battlesWon;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {battlesWon} won battles and {energy} energy");
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (command == "End of battle")
+            {
+                Console.WriteLine($"Won battles: {battlesWon}. Energy left: {energy}");
+            }
+        }
+    }
+}
