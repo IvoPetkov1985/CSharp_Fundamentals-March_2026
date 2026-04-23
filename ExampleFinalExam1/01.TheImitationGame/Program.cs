6e73971c57b1a10e3356f5960196b75349e450c1
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-23 16:35:06 +0300

    Tasks 1 and 2 uploaded.

---
+++ ExampleFinalExam1/01.TheImitationGame/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace _01.TheImitationGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            StringBuilder builder = new(message);
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Decode")
+            {
+                string[] arguments = inputLine
+                    .Split("|", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = arguments[0];
+
+                if (command == "Move")
+                {
+                    int lettersCount = int.Parse(arguments[1]);
+
+                    if (lettersCount > 0 && lettersCount < builder.Length)
+                    {
+                        string substring = builder.ToString().Substring(0, lettersCount);
+                        builder.Remove(0, lettersCount);
+                        builder.Append(substring);
+                    }
+                }
+                else if (command == "Insert")
+                {
+                    int index = int.Parse(arguments[1]);
+                    string value = arguments[2];
+
+                    if (index >= 0 && index <= builder.Length)
+                    {
+                        builder.Insert(index, value);
+                    }
+                }
+                else if (command == "ChangeAll")
+                {
+                    string oldSubstring = arguments[1];
+                    string replacement = arguments[2];
+                    builder.Replace(oldSubstring, replacement);
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {builder.ToString()}");
+        }
+    }
+}
---
+++ ExampleFinalExam1/02.AdAstra/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02.AdAstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            int neededCaloriesPerDay = 2000;
+            int totalCalories = 0;
+
+            Regex pattern = new(@"(#|\|)(?<product>[A-Za-z ]+)\1(?<date>\d{2}/\d{2}/\d{2})\1(?<calories>\d+)\1");
+            MatchCollection collection = pattern.Matches(inputText);
+
+            foreach (Match match in collection)
+            {
+                int calories = int.Parse(match.Groups["calories"].Value);
+                totalCalories += calories;
+            }
+
+            int daysLast = totalCalories / neededCaloriesPerDay;
+
+            StringBuilder builder = new();
+            builder.AppendLine($"You have food to last you for: {daysLast} days!");
+
+            if (collection.Any())
+            {
+                foreach (Match match in collection)
+                {
+                    string product = match.Groups["product"].Value;
+                    string bestBefore = match.Groups["date"].Value;
+                    int calories = int.Parse(match.Groups["calories"].Value);
+                    builder.AppendLine($"Item: {product}, Best before: {bestBefore}, Nutrition: {calories}");
+                }
+            }
+
+            Console.WriteLine(builder.ToString().TrimEnd());
+        }
+    }
+}
