40538fd7e289aee30b747eb2420aa15ba56f98bc
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-25 13:57:07 +0300

    Tasks 1 and 2 uploaded.

---
+++ RegularExam/01.GoogleSearches/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _01.GoogleSearches
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal moneyPerSearch = decimal.Parse(Console.ReadLine());
+            int usersCount = int.Parse(Console.ReadLine());
+            decimal totalMoney = 0;
+
+            for (int user = 1; user <= usersCount; user++)
+            {
+                int searches = int.Parse(Console.ReadLine());
+
+                if (searches <= 1)
+                {
+                    continue;
+                }
+
+                decimal currentMoney = searches * moneyPerSearch;
+
+                if (searches > 5)
+                {
+                    currentMoney *= 2;
+                }
+
+                if (user % 3 == 0)
+                {
+                    currentMoney *= 3;
+                }
+
+                totalMoney += currentMoney;
+            }
+
+            Console.WriteLine($"Total money earned: {totalMoney:F2}");
+        }
+    }
+}
---
+++ RegularExam/02.GeneratingNumbers/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace _02.GeneratingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "END")
+            {
+                string[] tokens = commandLine
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+
+                if (command == "add")
+                {
+                    int[] numbersToAdd = tokens.Skip(3).Select(int.Parse).ToArray();
+                    numbers.InsertRange(0, numbersToAdd);
+                }
+                else if (command == "remove")
+                {
+                    if (tokens[1] == "greater")
+                    {
+                        int value = int.Parse(tokens[3]);
+                        numbers.RemoveAll(x => x > value);
+                    }
+                    else if (tokens[1] == "at")
+                    {
+                        int index = int.Parse(tokens[3]);
+
+                        if (index >= 0 && index < numbers.Count)
+                        {
+                            numbers.RemoveAt(index);
+                        }
+                    }
+                }
+                else if (command == "replace")
+                {
+                    int valueToReplace = int.Parse(tokens[1]);
+                    int replacement = int.Parse(tokens[2]);
+                    int indexOfReplacement = numbers.IndexOf(valueToReplace);
+
+                    if (indexOfReplacement > -1)
+                    {
+                        numbers.RemoveAt(indexOfReplacement);
+                        numbers.Insert(indexOfReplacement, replacement);
+                    }
+                }
+                else if (command == "find")
+                {
+                    string numbersType = tokens[1];
+
+                    switch (numbersType)
+                    {
+                        case "even":
+
+                            List<int> evenNumbers = numbers.Where(x => x % 2 == 0).ToList();
+                            Console.WriteLine(string.Join(" ", evenNumbers));
+
+                            break;
+                        case "odd":
+
+                            List<int> oddNumbers = numbers.Where(x => x % 2 != 0).ToList();
+                            Console.WriteLine(string.Join(" ", oddNumbers));
+
+                            break;
+                    }
+                }
+
+                commandLine = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
