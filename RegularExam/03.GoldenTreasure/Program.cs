3d87e2e3a78183f732e919ac0474e61da6464f9d
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-25 13:57:49 +0300

    Tasks 3 and 4 uploaded.

---
+++ RegularExam/03.GoldenTreasure/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Text;
+
+namespace _03.GoldenTreasure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            string commandLine = Console.ReadLine();
+
+            StringBuilder builder = new();
+
+            while (commandLine != "Error")
+            {
+                string[] tokens = commandLine
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+
+                if (command == "Infuse")
+                {
+                    string charcters = tokens[1];
+                    builder.Append(charcters);
+                    Console.WriteLine(builder.ToString());
+                }
+                else if (command == "Transpose")
+                {
+                    int firstIndex = int.Parse(tokens[1]);
+                    int secondIndex = int.Parse(tokens[2]);
+
+                    if (firstIndex >= 0 && firstIndex < builder.Length &&
+                        secondIndex >= 0 && secondIndex < builder.Length)
+                    {
+                        char temp = builder[firstIndex];
+                        builder[firstIndex] = builder[secondIndex];
+                        builder[secondIndex] = temp;
+                        Console.WriteLine(builder.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed attempt to unlock the treasure");
+                    }
+                }
+                else if (command == "Retrace")
+                {
+                    int index = int.Parse(tokens[1]);
+                    string part = builder.ToString().Substring(0, index + 1);
+                    string reversed = new string(part.ToCharArray().Reverse().ToArray());
+                    builder.Remove(0, index + 1);
+                    builder.Insert(0, reversed);
+                    Console.WriteLine(builder.ToString());
+                }
+                else if (command == "Destroy")
+                {
+                    string character = tokens[1];
+
+                    if (builder.ToString().Contains(character))
+                    {
+                        builder.Replace(character, string.Empty);
+                        Console.WriteLine(builder.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Character {character} is invalid");
+                    }
+                }
+                else if (command == "Freeze")
+                {
+                    int counter = 0;
+                    bool isError = false;
+
+                    while (counter < 2)
+                    {
+                        commandLine = Console.ReadLine();
+
+                        if (commandLine == "Error")
+                        {
+                            isError = true;
+                            break;
+                        }
+
+                        counter++;
+                    }
+
+                    if (isError)
+                    {
+                        break;
+                    }
+                }
+
+                if (builder.ToString() == password)
+                {
+                    Console.WriteLine("The Golden Treasure has been unlocked!");
+                    break;
+                }
+
+                commandLine = Console.ReadLine();
+            }
+
+            if (commandLine == "Error")
+            {
+                Console.WriteLine($"The string {builder.ToString()} does not match the password {password}!");
+            }
+        }
+    }
+}
---
+++ RegularExam/04.MonthlyReport/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace _04.MonthlyReport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> clients = new();
+            Dictionary<string, double> distributors = new();
+
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "End")
+            {
+                string[] tokens = commandLine
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+                string name = tokens[1];
+                double value = double.Parse(tokens[2]);
+
+                if (command == "Deliver")
+                {
+                    if (!distributors.ContainsKey(name))
+                    {
+                        distributors.Add(name, 0);
+                    }
+
+                    distributors[name] += value;
+                }
+                else if (command == "Return")
+                {
+                    if (distributors.ContainsKey(name))
+                    {
+                        if (value <= distributors[name])
+                        {
+                            distributors[name] -= value;
+
+                            if (distributors[name] == 0)
+                            {
+                                distributors.Remove(name);
+                            }
+                        }
+                    }
+                }
+                else if (command == "Sell")
+                {
+                    if (!clients.ContainsKey(name))
+                    {
+                        clients.Add(name, 0);
+                    }
+
+                    clients[name] += value;
+                }
+
+                commandLine = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, double> client in clients)
+            {
+                Console.WriteLine($"{client.Key}: {client.Value:F2}");
+            }
+
+            Console.WriteLine("-----------");
+
+            foreach (KeyValuePair<string, double> distributor in distributors)
+            {
+                Console.WriteLine($"{distributor.Key}: {distributor.Value:F2}");
+            }
+
+            Console.WriteLine("-----------");
+
+            double totalIncome = 0;
+
+            if (clients.Count > 0)
+            {
+                totalIncome = clients.Values.Sum();
+            }
+
+            Console.WriteLine($"Total Income: {totalIncome:F2}");
+        }
+    }
+}
