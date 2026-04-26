b18c5ab078e36122e98b946b79711ea6088ae5e9
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-26 18:41:00 +0300

    Tasks 1 and 2 uploaded.

---
+++ ExampleFinalExam4/01.PasswordReset/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _01.PasswordReset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Done")
+            {
+                string[] arguments = inputLine
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = arguments[0];
+
+                switch (command)
+                {
+                    case "TakeOdd":
+                        char[] passwordAsArr = password.ToCharArray();
+                        passwordAsArr = passwordAsArr.Where((x, i) => i % 2 != 0).ToArray();
+                        password = new string(passwordAsArr);
+                        Console.WriteLine(password);
+                        break;
+                    case "Cut":
+                        int startIndex = int.Parse(arguments[1]);
+                        int count = int.Parse(arguments[2]);
+                        password = password.Remove(startIndex, count);
+                        Console.WriteLine(password);
+                        break;
+                    case "Substitute":
+                        string oldSubstring = arguments[1];
+                        string newSubstring = arguments[2];
+
+                        if (password.Contains(oldSubstring))
+                        {
+                            password = password.Replace(oldSubstring, newSubstring);
+                            Console.WriteLine(password);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nothing to replace!");
+                        }
+
+                        break;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your password is: {password}");
+        }
+    }
+}
---
+++ ExampleFinalExam4/02.FancyBarcodes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.FancyBarcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int barcodesCount = int.Parse(Console.ReadLine());
+            Regex regex = new Regex(@"(@#+)(?<item>[A-Z][A-Za-z0-9]{4,}[A-Z])(@#+)");
+            Regex digitRegex = new Regex(@"\d");
+
+            for (int i = 0; i < barcodesCount; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string product = match.Groups["item"].Value;
+                    string productGroup = "00";
+                    MatchCollection digits = digitRegex.Matches(product);
+
+                    if (digits.Count > 0)
+                    {
+                        productGroup = string.Join("", digits);
+                    }
+
+                    Console.WriteLine($"Product group: {productGroup}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
