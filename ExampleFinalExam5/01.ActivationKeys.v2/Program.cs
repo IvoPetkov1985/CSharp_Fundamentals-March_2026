b56597398e0d74a6964bf602d4ed83c1b4c97565
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-27 14:50:20 +0300

    Task 1 uploaded.
    Activation Keys with two solutions.

---
+++ ExampleFinalExam5/01.ActivationKeys.v2/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _01.ActivationKeys.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "Generate")
+            {
+                string[] tokens = commandLine
+                    .Split(">>>", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+
+                if (command == "Contains")
+                {
+                    string searchedSubstring = tokens[1];
+
+                    if (activationKey.Contains(searchedSubstring))
+                    {
+                        Console.WriteLine($"{activationKey} contains {searchedSubstring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (command == "Flip")
+                {
+                    string casing = tokens[1];
+                    int startIndex = int.Parse(tokens[2]);
+                    int endIndex = int.Parse(tokens[3]);
+                    string substring = activationKey.Substring(startIndex, endIndex - startIndex);
+                    activationKey = activationKey.Remove(startIndex, endIndex - startIndex);
+
+                    switch (casing)
+                    {
+                        case "Upper":
+                            substring = substring.ToUpper();
+                            break;
+                        case "Lower":
+                            substring = substring.ToLower();
+                            break;
+                    }
+
+                    activationKey = activationKey.Insert(startIndex, substring);
+                    Console.WriteLine(activationKey);
+                }
+                else if (command == "Slice")
+                {
+                    int startIndex = int.Parse(tokens[1]);
+                    int endIndex = int.Parse(tokens[2]);
+                    activationKey = activationKey.Remove(startIndex, endIndex - startIndex);
+                    Console.WriteLine(activationKey);
+                }
+
+                commandLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your activation key is: {activationKey}");
+        }
+    }
+}
---
+++ ExampleFinalExam5/01.ActivationKeys/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace _01.ActivationKeys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+            StringBuilder builder = new(activationKey);
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "Generate")
+            {
+                string[] arguments = commandLine
+                    .Split(">>>", StringSplitOptions.RemoveEmptyEntries);
+                string command = arguments.First();
+
+                if (command == "Contains")
+                {
+                    string substring = arguments[1];
+
+                    if (builder.ToString().Contains(substring))
+                    {
+                        Console.WriteLine($"{builder.ToString()} contains {substring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (command == "Flip")
+                {
+                    string flipType = arguments[1];
+                    int startIndex = int.Parse(arguments[2]);
+                    int endIndex = int.Parse(arguments[3]);
+                    string partToChange = builder.ToString().Substring(startIndex, endIndex - startIndex);
+                    builder.Remove(startIndex, endIndex - startIndex);
+
+                    switch (flipType)
+                    {
+                        case "Upper":
+                            partToChange = partToChange.ToUpper();
+                            break;
+                        case "Lower":
+                            partToChange = partToChange.ToLower();
+                            break;
+                    }
+
+                    builder.Insert(startIndex, partToChange);
+                    Console.WriteLine(builder.ToString());
+                }
+                else if (command == "Slice")
+                {
+                    int startIndex = int.Parse(arguments[1]);
+                    int endIndex = int.Parse(arguments[2]);
+                    builder.Remove(startIndex, endIndex - startIndex);
+                    Console.WriteLine(builder.ToString());
+                }
+
+                commandLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your activation key is: {builder.ToString()}");
+        }
+    }
+}
