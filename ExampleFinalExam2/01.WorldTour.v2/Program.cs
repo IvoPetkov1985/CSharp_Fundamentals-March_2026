1c857eae98fdc40e044436c0217de0f8fcccad44
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-27 14:47:27 +0300

    Tasks 1 and 2 uploaded.
    Another solutions added.

---
+++ ExampleFinalExam2/01.WorldTour.v2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace _01.WorldTour.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stops = Console.ReadLine();
+            StringBuilder builder = new(stops);
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "Travel")
+            {
+                string[] tokens = commandLine
+                    .Split(":", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+
+                if (command == "Add Stop")
+                {
+                    int index = int.Parse(tokens[1]);
+                    string substring = tokens[2];
+
+                    if (index >= 0 && index <= builder.Length)
+                    {
+                        builder.Insert(index, substring);
+                    }
+                }
+                else if (command == "Remove Stop")
+                {
+                    int startIndex = int.Parse(tokens[1]);
+                    int endIndex = int.Parse(tokens[2]);
+
+                    if (startIndex >= 0 && startIndex < builder.Length &&
+                        endIndex >= 0 && endIndex < builder.Length &&
+                        endIndex >= startIndex)
+                    {
+                        builder.Remove(startIndex, endIndex - startIndex + 1);
+                    }
+                }
+                else if (command == "Switch")
+                {
+                    string oldSubstring = tokens[1];
+                    string newSubstring = tokens[2];
+                    builder.Replace(oldSubstring, newSubstring);
+                }
+
+                Console.WriteLine(builder.ToString());
+                commandLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {builder.ToString()}");
+        }
+    }
+}
---
+++ ExampleFinalExam2/02.DestinationMapper.v3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper.v3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            Regex countryPattern = new(@"([=\/])([A-Z][A-Za-z]{2,})\1");
+            Dictionary<string, int> countriesPoints = new();
+
+            MatchCollection destinationMatches = countryPattern.Matches(inputText);
+
+            foreach (Match match in destinationMatches)
+            {
+                string countryName = match.Groups[2].Value;
+                countriesPoints.Add(countryName, countryName.Length);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", countriesPoints.Keys)}");
+            Console.WriteLine($"Travel Points: {countriesPoints.Values.Sum()}");
+        }
+    }
+}
