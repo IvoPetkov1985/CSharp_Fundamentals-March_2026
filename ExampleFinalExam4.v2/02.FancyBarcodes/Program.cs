695351bcf0d3c4e5c23d8d56935d224b37c993c2
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-28 10:36:23 +0300

    Task 2 uploaded.

---
+++ ExampleFinalExam4.v2/02.FancyBarcodes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.FancyBarcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int barcodesCount = int.Parse(Console.ReadLine());
+            Regex pattern = new(@"^(@#+)([A-Z][A-Za-z0-9]{4,}[A-Z])(@#+)$");
+            Regex digitPattern = new(@"\d");
+
+            for (int i = 0; i < barcodesCount; i++)
+            {
+                string barcode = Console.ReadLine();
+                Match match = pattern.Match(barcode);
+
+                if (match.Success)
+                {
+                    string product = match.Groups[2].Value;
+                    string productGroup = "00";
+                    MatchCollection digitCollection = digitPattern.Matches(product);
+
+                    if (digitCollection.Count > 0)
+                    {
+                        productGroup = string.Join(string.Empty, digitCollection);
+                    }
+
+                    Console.WriteLine($"Product group: {productGroup}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
