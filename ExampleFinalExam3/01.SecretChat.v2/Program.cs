6430591c5cc4f5e96e851dbfe8d10b90695cf7e5
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-29 14:05:49 +0300

    Tasks 1 and 2 uploaded.
    Additional solutions added.

---
+++ ExampleFinalExam3/01.SecretChat.v2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace _01.SecretChat.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string concealedMessage = Console.ReadLine();
+            StringBuilder message = new(concealedMessage);
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Reveal")
+            {
+                string[] arguments = inputLine
+                    .Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+                string command = arguments[0];
+
+                if (command == "InsertSpace")
+                {
+                    int insertIndex = int.Parse(arguments[1]);
+                    message.Insert(insertIndex, " ");
+                    Console.WriteLine(message);
+                }
+                else if (command == "Reverse")
+                {
+                    string substringToReverse = arguments[1];
+
+                    if (message.ToString().Contains(substringToReverse))
+                    {
+                        int startIndex = message.ToString().IndexOf(substringToReverse);
+                        message.Remove(startIndex, substringToReverse.Length);
+                        string reversed = new string(substringToReverse.Reverse().ToArray());
+                        message.Append(reversed);
+                        Console.WriteLine(message);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+                else if (command == "ChangeAll")
+                {
+                    string oldSubstring = arguments[1];
+                    string newSubstring = arguments[2];
+                    message.Replace(oldSubstring, newSubstring);
+                    Console.WriteLine(message);
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+    }
+}
---
+++ ExampleFinalExam3/02.MirrorWords.v2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.MirrorWords.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            Regex pairPattern = new(@"([\@\#])([A-Za-z]{3,})\1\1([A-Za-z]{3,})\1");
+            List<string> mirrorWords = new();
+
+            MatchCollection pairs = pairPattern.Matches(inputText);
+
+            foreach (Match match in pairs)
+            {
+                string firstWord = match.Groups[2].Value;
+                string secondWord = match.Groups[3].Value;
+                string reversed = new string(secondWord.Reverse().ToArray());
+
+                if (firstWord == reversed)
+                {
+                    mirrorWords.Add($"{firstWord} <=> {secondWord}");
+                }
+            }
+
+            if (pairs.Count > 0)
+            {
+                Console.WriteLine($"{pairs.Count} word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+
+            if (mirrorWords.Count > 0)
+            {
+                Console.WriteLine("The mirror words are:");
+                Console.WriteLine(string.Join(", ", mirrorWords));
+            }
+            else
+            {
+                Console.WriteLine("No mirror words!");
+            }
+        }
+    }
+}
