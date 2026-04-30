9bf9ed8d54410991c763e312c1e4c0f49a1ad384
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-30 14:20:05 +0300

    Task 1 uploaded.
    Two solutions of Registration.

---
+++ ExampleFinalExam0/01.Registration.v2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Text;
+
+namespace _01.Registration.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            StringBuilder builder = new(username);
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Registration")
+            {
+                string[] arguments = inputLine
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = arguments[0];
+
+                switch (command)
+                {
+                    case "Letters":
+                        string lettersType = arguments[1];
+                        string converted = string.Empty;
+
+                        if (lettersType == "Lower")
+                        {
+                            converted = builder.ToString().ToLower();
+                        }
+                        else if (lettersType == "Upper")
+                        {
+                            converted = builder.ToString().ToUpper();
+                        }
+
+                        builder.Clear();
+                        builder.Append(converted);
+                        Console.WriteLine(builder);
+                        break;
+                    case "Reverse":
+                        int startIndex = int.Parse(arguments[1]);
+                        int endIndex = int.Parse(arguments[2]);
+
+                        if (startIndex >= 0 && startIndex < builder.Length &&
+                            endIndex >= 0 && endIndex < builder.Length &&
+                            endIndex >= startIndex)
+                        {
+                            string partToReverse = builder.ToString()
+                                .Substring(startIndex, endIndex - startIndex + 1);
+                            string reversed = new string(partToReverse.Reverse().ToArray());
+                            Console.WriteLine(reversed);
+                        }
+
+                        break;
+                    case "Substring":
+                        string substringToCut = arguments[1];
+                        int index = builder.ToString().IndexOf(substringToCut);
+
+                        if (index != -1)
+                        {
+                            builder.Remove(index, substringToCut.Length);
+                            Console.WriteLine(builder);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"The username {builder} doesn't contain {substringToCut}.");
+                        }
+
+                        break;
+                    case "Replace":
+                        char charToReplace = char.Parse(arguments[1]);
+                        builder.Replace(charToReplace, '-');
+                        Console.WriteLine(builder);
+                        break;
+                    case "IsValid":
+                        char validateChar = char.Parse(arguments[1]);
+                        if (builder.ToString().Contains(validateChar))
+                        {
+                            Console.WriteLine("Valid username.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{validateChar} must be contained in your username.");
+                        }
+                        break;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ExampleFinalExam0/01.Registration/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace _01.Registration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string desiredUsername = Console.ReadLine();
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "Registration")
+            {
+                string[] arguments = commandLine
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = arguments[0];
+
+                if (command == "Letters")
+                {
+                    string letterType = arguments[1];
+
+                    switch (letterType)
+                    {
+                        case "Lower":
+                            desiredUsername = desiredUsername.ToLower();
+                            break;
+                        case "Upper":
+                            desiredUsername = desiredUsername.ToUpper();
+                            break;
+                    }
+
+                    Console.WriteLine(desiredUsername);
+                }
+                else if (command == "Reverse")
+                {
+                    int startIndex = int.Parse(arguments[1]);
+                    int endIndex = int.Parse(arguments[2]);
+
+                    if (startIndex >= 0 && startIndex < desiredUsername.Length &&
+                        endIndex >= 0 && endIndex < desiredUsername.Length &&
+                        endIndex >= startIndex)
+                    {
+                        string substring = desiredUsername.Substring(startIndex, endIndex - startIndex + 1);
+                        string reversed = new string(substring.Reverse().ToArray());
+                        Console.WriteLine(reversed);
+                    }
+                }
+                else if (command == "Substring")
+                {
+                    string substring = arguments[1];
+
+                    if (desiredUsername.Contains(substring))
+                    {
+                        int startIndex = desiredUsername.IndexOf(substring);
+                        desiredUsername = desiredUsername.Remove(startIndex, substring.Length);
+                        Console.WriteLine(desiredUsername);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The username {desiredUsername} doesn't contain {substring}.");
+                    }
+                }
+                else if (command == "Replace")
+                {
+                    char charToReplace = char.Parse(arguments[1]);
+                    desiredUsername = desiredUsername.Replace(charToReplace, '-');
+                    Console.WriteLine(desiredUsername);
+                }
+                else if (command == "IsValid")
+                {
+                    char charToValidate = char.Parse(arguments[1]);
+
+                    if (desiredUsername.Contains(charToValidate))
+                    {
+                        Console.WriteLine("Valid username.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{charToValidate} must be contained in your username.");
+                    }
+                }
+
+                commandLine = Console.ReadLine();
+            }
+        }
+    }
+}
