ddbd204ee0b7918c2cf45cb71e5e6dc6e12139a0
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-30 14:20:44 +0300

    Task 2 uploaded.

---
+++ ExampleFinalExam0/02.DestinationMapper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            Regex pattern = new(@"(=|\/)([A-Z][A-Za-z]{2,})\1");
+            Dictionary<string, int> destinationsPoints = new();
+
+            MatchCollection collection = pattern.Matches(inputText);
+
+            foreach (Match match in collection)
+            {
+                string destination = match.Groups[2].Value;
+                int points = destination.Length;
+                destinationsPoints.TryAdd(destination, points);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destinationsPoints.Keys)}");
+            Console.WriteLine($"Travel Points: {destinationsPoints.Values.Sum()}");
+        }
+    }
+}
