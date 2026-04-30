1c44181479674af6ba746c423871c00cb8dbc56e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-04-30 14:21:34 +0300

    Task 3 uploaded.
    Two solutions of Bakery Shop. sln file added.

---
+++ ExampleFinalExam0/03.BakeryShop.v2/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _03.BakeryShop.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> foods = new();
+            List<int> sells = new();
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Complete")
+            {
+                string[] arguments = inputLine
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = arguments[0];
+                int quantity = int.Parse(arguments[1]);
+                string food = arguments[2];
+
+                switch (command)
+                {
+                    case "Receive":
+
+                        if (quantity > 0)
+                        {
+                            foods.TryAdd(food, 0);
+                            foods[food] += quantity;
+                        }
+
+                        break;
+                    case "Sell":
+
+                        if (foods.ContainsKey(food))
+                        {
+                            if (quantity <= foods[food])
+                            {
+                                foods[food] -= quantity;
+                                sells.Add(quantity);
+                                Console.WriteLine($"You sold {quantity} {food}.");
+
+                                if (foods[food] == 0)
+                                {
+                                    foods.Remove(food);
+                                }
+                            }
+                            else
+                            {
+                                sells.Add(foods[food]);
+                                Console.WriteLine($"There aren't enough {food}. You sold the last {foods[food]} of them.");
+                                foods.Remove(food);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You do not have any {food}.");
+                        }
+
+                        break;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, int> foodInfo in foods)
+            {
+                Console.WriteLine($"{foodInfo.Key}: {foodInfo.Value}");
+            }
+
+            Console.WriteLine($"All sold: {sells.Sum()} goods");
+        }
+    }
+}
---
+++ ExampleFinalExam0/03.BakeryShop/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _03.BakeryShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> foodsQuantities = new();
+            int soldFoods = 0;
+
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "Complete")
+            {
+                string[] tokens = commandLine
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+                int quantity = int.Parse(tokens[1]);
+                string food = tokens[2];
+
+                if (command == "Receive")
+                {
+                    if (quantity > 0)
+                    {
+                        if (foodsQuantities.ContainsKey(food) == false)
+                        {
+                            foodsQuantities.Add(food, 0);
+                        }
+
+                        foodsQuantities[food] += quantity;
+                    }
+                }
+                else if (command == "Sell")
+                {
+                    if (foodsQuantities.ContainsKey(food) == false)
+                    {
+                        Console.WriteLine($"You do not have any {food}.");
+                    }
+                    else
+                    {
+                        if (foodsQuantities[food] >= quantity)
+                        {
+                            foodsQuantities[food] -= quantity;
+                            soldFoods += quantity;
+                            Console.WriteLine($"You sold {quantity} {food}.");
+
+                            if (foodsQuantities[food] == 0)
+                            {
+                                foodsQuantities.Remove(food);
+                            }
+                        }
+                        else
+                        {
+                            soldFoods += foodsQuantities[food];
+                            Console.WriteLine($"There aren't enough {food}. You sold the last {foodsQuantities[food]} of them.");
+                            foodsQuantities[food] = 0;
+                            foodsQuantities.Remove(food);
+                        }
+                    }
+                }
+
+                commandLine = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, int> food in foodsQuantities)
+            {
+                Console.WriteLine($"{food.Key}: {food.Value}");
+            }
+
+            Console.WriteLine($"All sold: {soldFoods} goods");
+        }
+    }
+}
