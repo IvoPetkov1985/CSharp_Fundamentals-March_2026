e8f8e79d2a6a18886d1897692639a175287494f6
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-01 12:11:38 +0300

    Task 1 uploaded.

---
+++ ExampleMidExam3/01.CounterStrike.v2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _01.CounterStrike.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            int battlesWon = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "End of battle")
+                {
+                    Console.WriteLine($"Won battles: {battlesWon}. Energy left: {energy}");
+                    break;
+                }
+
+                int distance = int.Parse(command);
+
+                if (energy >= distance)
+                {
+                    energy -= distance;
+                    battlesWon++;
+
+                    if (battlesWon % 3 == 0)
+                    {
+                        energy += battlesWon;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {battlesWon} won battles and {energy} energy");
+                    break;
+                }
+            }
+        }
+    }
+}
