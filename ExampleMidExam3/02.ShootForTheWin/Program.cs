9d408b0fa7ff7e98b38e25a262ba0ece4a452eb2
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-03 12:25:44 +0300

    Example Mid Exam 3 uploaded.

---
+++ ExampleMidExam3/02.ShootForTheWin/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _02.ShootForTheWin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] targets = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int targetsShot = 0;
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                int index = int.Parse(input);
+
+                if (index < 0 || index >= targets.Length)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                if (targets[index] != -1)
+                {
+                    targetsShot++;
+                    int value = targets[index];
+                    targets[index] = -1;
+
+                    for (int i = 0; i < targets.Length; i++)
+                    {
+                        if (targets[i] == -1)
+                        {
+                            continue;
+                        }
+
+                        if (targets[i] > value)
+                        {
+                            targets[i] -= value;
+                        }
+                        else
+                        {
+                            targets[i] += value;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Shot targets: {targetsShot} -> {string.Join(" ", targets)}");
+        }
+    }
+}
